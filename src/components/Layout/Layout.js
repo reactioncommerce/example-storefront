165e63eedf25a6da72392a3448f48d78175a92dd
Mike Murray <hellomikemurray@gmail.com>
2018-04-05 09:49:40 -0700

    refactor: move Layout to a separate file

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+import Header from "components/Header";
+
+export default ({ children, title = '' }) => (
+  <div>
+    <Head>
+      <title key="title">{title}</title>
+    </Head>
+    <Header />
+
+    {children}
+
+  </div>
+)
