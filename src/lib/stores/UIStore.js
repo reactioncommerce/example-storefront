3ce069dc192f73a7e6b14e461e547084304a0d3a
Mike Murray <hellomikemurray@gmail.com>
2018-04-06 14:26:42 -0700

    feat: add navigation drawer state

--- src/lib/stores/UIStore.js
+++ src/lib/stores/UIStore.js
@@ -2,6 +2,7 @@ import { observable, computed, action } from "mobx";
 
 class UIStore {
   @observable _cartOpen = false;
+  @observable _menuDrawerOpen = false;
 
   @computed get cartOpen() {
     return this._cartOpen;
@@ -13,6 +14,18 @@ class UIStore {
 
   @action toggleCartOpen = () => {
     this.cartOpen = !this.cartOpen;
+  }
+
+  @computed get menuDrawerOpen() {
+    return this._menuDrawerOpen;
+  }
+
+  set menuDrawerOpen(value) {
+    this._menuDrawerOpen = value;
+  }
+
+  @action toggleMenuDrawerOpen = () => {
+    this.menuDrawerOpen = !this.menuDrawerOpen;
   }
 }
 
