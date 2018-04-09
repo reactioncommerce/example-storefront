0b9870d165142b5efedf3a84bd5dcac6956b7403
Mike Murray <hellomikemurray@gmail.com>
2018-04-09 09:58:35 -0700

    test: add VerticalNavigationItem test

---
+++ src/components/NavigationItem/VerticalNavigationItem.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import VerticalNavigationItem from "./VerticalNavigationItem";
+
+const testMenuItem = {
+  _id: "123",
+  name: "Shop",
+  slug: "shop"
+}
+
+const testMenuItemWithRelatedTags = {
+  _id: "123",
+  name: "Shop",
+  slug: "shop",
+  relatedTags: [
+    { _id: "111", name: "Clothes", slug: "clothes" },
+    { _id: "222", name: "Shoes", slug: "shoes" },
+    { _id: "333", name: "Accessories", slug: "accessories" }
+  ]
+}
+
+test("basic snapshot", () => {
+  const component = renderer.create(<VerticalNavigationItem menuItem={testMenuItem} />);
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("snapshot with related items", () => {
+  const component = renderer.create(<VerticalNavigationItem menuItem={testMenuItemWithRelatedTags} />);
+
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
