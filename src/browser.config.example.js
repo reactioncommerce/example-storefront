34a846d8a59256414a10a17c2244ceb798fdd7bc
Mike Murray <hellomikemurray@gmail.com>
2018-04-13 11:14:01 -0700

    feat: add browser config file example

---
+++ src/browser.config.example.js
@@ -0,0 +1,5 @@
+// Browser config
+export default {
+  GRAPHQL_URL: "http://devserver.reaction-api:3030/graphql-alpha",
+  METEOR_TOKEN: "${get the Reaction meteor.loginToken from local-storage}"
+};
