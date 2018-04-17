91b8130614a85ef0870f083eb929a37ffa7fbaaf
Mike Murray <hellomikemurray@gmail.com>
2018-04-17 15:35:07 -0700

    feat: add auth store to root store object

--- src/lib/stores/index.js
+++ src/lib/stores/index.js
@@ -1,8 +1,10 @@
 import { configure } from "mobx";
+import AuthStore from "./AuthStore";
 import UIStore from "./UIStore";
 
 configure({ enforceActions: true });
 
 export default {
+  authStore: new AuthStore(),
   uiStore: new UIStore()
 };
