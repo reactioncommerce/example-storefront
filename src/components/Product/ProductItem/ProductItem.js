86b10a48e8663bdbfd84db2aad836de8a2dfcaf1
Nat Hamilton <info@nathamilton.com>
2018-04-17 14:39:14 -0500

    chore: moved ProductItems styles into own file

--- src/components/Product/ProductItem/ProductItem.js
+++ src/components/Product/ProductItem/ProductItem.js
@@ -9,6 +9,8 @@ import Hidden from "material-ui/Hidden";
 import Typography from "material-ui/Typography";
 import LoadingIcon from "mdi-material-ui/Loading";
 
+import { styles } from "./styles";
+
 // TODO: random number for temp images, REMOVE ONCE WE HAVE REAL DATA
 const tempImgs = {
   0: {
@@ -30,92 +32,6 @@ const tempImgs = {
     lg: "http://via.placeholder.com/1600x512/E6E6E6/999999?text=FPO"
   }
 };
-
-const styles = theme => ({
-  [`@keyframes spin`]: {
-    from: { transform: "rotate(0deg)" },
-    to: { transform: "rotate(-360deg)" }
-  },
-  root: {},
-  productInfo: {
-    display: "flex",
-    justifyContent: "space-between"
-  },
-  productMedia: {
-    backgroundColor: theme.palette.primary.contrastText,
-    position: "relative"
-  },
-  chip: {
-    ...theme.typography.caption,
-    borderRadius: 4,
-    height: "auto",
-    fontSize: 12,
-    paddingBottom: theme.spacing.unit * 0.5,
-    paddingLeft: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingTop: theme.spacing.unit * 0.5,
-    position: "absolute",
-    zIndex: 100
-  },
-  chipLabel: {
-    fontWeight: theme.typography.fontWeightBold,
-    padding: 0
-  },
-  status: {
-    color: theme.palette.primary.contrastText,
-    left: theme.spacing.unit,
-    top: theme.spacing.unit
-  },
-  statusSoldOut: {
-    backgroundColor: theme.palette.secondary.main
-  },
-  statusBackorder: {
-    backgroundColor: theme.palette.secondary.dark
-  },
-  statusSale: {
-    backgroundColor: theme.palette.error.main
-  },
-  statusBestseller: {
-    backgroundColor: theme.palette.primary.light
-  },
-  warning: {
-    backgroundColor: "transparent",
-    right: theme.spacing.unit,
-    top: theme.spacing.unit
-  },
-  img: {
-    height: "auto",
-    width: "100%"
-  },
-  imgLoading: {
-    alignItems: "center",
-    backgroundColor: "transparent",
-    display: "flex",
-    fontSize: 48,
-    height: 100,
-    justifyContent: "center",
-    left: "calc(50% - 50px)",
-    position: "absolute",
-    top: "calc(50% - 50px)",
-    width: 100
-  },
-  loadingIcon: {
-    color: theme.palette.primary.light,
-    fontSize: "inherit",
-    animationName: "spin",
-    animationDuration: theme.transitions.duration.standard * 2,
-    animationTimingFunction: theme.transitions.easing.sharp,
-    animationIterationCount: "infinite",
-    animationFillMode: "both"
-  },
-  link: {
-    ...theme.typography.body2,
-    textAlign: "left",
-    "&:hover": {
-      color: theme.palette.secondary.main
-    }
-  }
-});
 
 @withStyles(styles)
 @withTheme()
---
+++ src/components/Product/ProductItem/styles.js
@@ -0,0 +1,85 @@
+export const styles = theme => ({
+  "@keyframes spin": {
+    from: { transform: "rotate(0deg)" },
+    to: { transform: "rotate(-360deg)" }
+  },
+  root: {},
+  productInfo: {
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  productMedia: {
+    backgroundColor: theme.palette.primary.contrastText,
+    position: "relative"
+  },
+  chip: {
+    ...theme.typography.caption,
+    borderRadius: 4,
+    height: "auto",
+    fontSize: 12,
+    paddingBottom: theme.spacing.unit * 0.5,
+    paddingLeft: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingTop: theme.spacing.unit * 0.5,
+    position: "absolute",
+    zIndex: 100
+  },
+  chipLabel: {
+    fontWeight: theme.typography.fontWeightBold,
+    padding: 0
+  },
+  status: {
+    color: theme.palette.primary.contrastText,
+    left: theme.spacing.unit,
+    top: theme.spacing.unit
+  },
+  statusSoldOut: {
+    backgroundColor: theme.palette.secondary.main
+  },
+  statusBackorder: {
+    backgroundColor: theme.palette.secondary.dark
+  },
+  statusSale: {
+    backgroundColor: theme.palette.error.main
+  },
+  statusBestseller: {
+    backgroundColor: theme.palette.primary.light
+  },
+  warning: {
+    backgroundColor: "transparent",
+    right: theme.spacing.unit,
+    top: theme.spacing.unit
+  },
+  img: {
+    height: "auto",
+    width: "100%"
+  },
+  imgLoading: {
+    alignItems: "center",
+    backgroundColor: "transparent",
+    display: "flex",
+    fontSize: 48,
+    height: 100,
+    justifyContent: "center",
+    left: "calc(50% - 50px)",
+    position: "absolute",
+    top: "calc(50% - 50px)",
+    width: 100
+  },
+  loadingIcon: {
+    color: theme.palette.primary.light,
+    fontSize: "inherit",
+    animationName: "spin",
+    animationDuration: theme.transitions.duration.standard * 2,
+    animationTimingFunction: theme.transitions.easing.sharp,
+    animationIterationCount: "infinite",
+    animationFillMode: "both"
+  },
+  link: {
+    ...theme.typography.body2,
+    textAlign: "left",
+    "&:hover": {
+      color: theme.palette.secondary.main
+    }
+  }
+});
