7970e08a1214b1040bc075a3207263f26738ea30
Mike Murray <hellomikemurray@gmail.com>
2018-04-18 13:43:29 -0700

    feat: add product route for a shop

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const routes = require("next-routes")();
 
 routes
   .add("product", "/product/:slug", "product")
+  .add("product", "/shop/:shopSlug/product/:productSlug", "product")
   .add("shop", "/shop/:shopId/:tag", "index")
   .add("tag", "/tag/:slug", "index");
 
