4b3001903c385ba0dde0f502bcc98f207ec2c3f8
Mike Murray <hellomikemurray@gmail.com>
2018-04-18 13:44:33 -0700

    refactor: update param name

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 const routes = require("next-routes")();
 
 routes
-  .add("product", "/product/:slug", "product")
   .add("product", "/shop/:shopSlug/product/:productSlug", "product")
+  .add("product", "/product/:productSlug", "product")
   .add("shop", "/shop/:shopId/:tag", "index")
   .add("tag", "/tag/:slug", "index");
 
