0bc601893acec44501bd9a8c65e843330d65d3c4
Nat Hamilton <info@nathamilton.com>
2018-04-19 10:55:34 -0500

    test: updated NavigationItem test to have better mock tags, update NavigationItem snapshots

--- src/components/NavigationDesktop/NavigationItemDesktop.test.js
+++ src/components/NavigationDesktop/NavigationItemDesktop.test.js
@@ -3,28 +3,129 @@ import renderer from "react-test-renderer";
 import NavigationItemDesktop from "./NavigationItemDesktop";
 
 const mockTag = {
-  _id: "123",
-  name: "Shop",
-  slug: "shop"
-};
-
-const mockTagWithSubTags = {
-  _id: "123",
-  name: "Shop",
-  slug: "shop",
+  _id: "cmVhY3Rpb24vdGFnOjJFcEdxYUx2TDRmNkFYRVhC",
+  position: null,
+  slug: "tag-slug",
+  name: "Tag Name",
   subTags: {
     edges: []
   }
 };
 
-test("basic snapshot", () => {
+const mockTagWithSubTags = {
+  _id: "cmVhY3Rpb24vdGFnOjVXZE5LYXprejZ5TWdrcGY5",
+  position: null,
+  slug: "tag-slug",
+  name: "Tag Name",
+  subTags: {
+    edges: [
+      {
+        node: {
+          _id: "cmVhY3Rpb24vdGFnOlRFdjdSV1pDTHl1d1dmOWs2",
+          position: null,
+          slug: "sub-tag-slug",
+          name: "Sub Tag Name",
+          subTags: {
+            edges: []
+          }
+        }
+      },
+      {
+        node: {
+          _id: "cmVhY3Rpb24vdGFnOmJ2MmVEbzV3V3lvNUdNMlN3",
+          position: null,
+          slug: "sub-tag-slug",
+          name: "Sub Tag name",
+          subTags: {
+            edges: []
+          }
+        }
+      }
+    ]
+  }
+};
+
+const mockTagsWithMoreSubTags = {
+  _id: "cmVhY3Rpb24vdGFnOkdyZ1dkYXVwOVB1ZlBYNEp6",
+  position: null,
+  slug: "tag-slug",
+  name: "Tag Name",
+  subTags: {
+    edges: [
+      {
+        node: {
+          _id: "cmVhY3Rpb24vdGFnOjVXZE5LYXprejZ5TWdrcGY5",
+          position: null,
+          slug: "sub-tag-slug",
+          name: "Sub Tag Slug",
+          subTags: {
+            edges: [
+              {
+                node: {
+                  _id: "cmVhY3Rpb24vdGFnOlRFdjdSV1pDTHl1d1dmOWs2",
+                  position: null,
+                  slug: "sub-sub-tag-slug",
+                  name: "Sub Sub Tag name"
+                }
+              },
+              {
+                node: {
+                  _id: "cmVhY3Rpb24vdGFnOmJ2MmVEbzV3V3lvNUdNMlN3",
+                  position: null,
+                  slug: "sub-sub-tag-slug",
+                  name: "Sub Sub Tag Name"
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        node: {
+          _id: "cmVhY3Rpb24vdGFnOkZHNnRLdkNvZGVId21Qb05p",
+          position: null,
+          slug: "sub-tag-slug",
+          name: "Sub Tag Name",
+          subTags: {
+            edges: [
+              {
+                node: {
+                  _id: "cmVhY3Rpb24vdGFnOjREelpZdG1ydTVZaXFYSHR3",
+                  position: null,
+                  slug: "sub-sub-tag-slug",
+                  name: "Sub Sub Tag Name"
+                }
+              },
+              {
+                node: {
+                  _id: "cmVhY3Rpb24vdGFnOjg3aW1wYTNKUDVhWVFKalJE",
+                  position: null,
+                  slug: "sub-sub-tag-slug",
+                  name: "Sub Sub Tag Name"
+                }
+              }
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
+
+test("renders with 1 level of tags", () => {
   const component = renderer.create(<NavigationItemDesktop navItems={mockTag} />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
-test("snapshot with related items", () => {
+test("renders with 2 levels of tags", () => {
   const component = renderer.create(<NavigationItemDesktop navItems={mockTagWithSubTags} />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("renders with 3 levels of tags", () => {
+  const component = renderer.create(<NavigationItemDesktop navItems={mockTagsWithMoreSubTags} />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/components/NavigationMobile/NavigationItemMobile.test.js
+++ src/components/NavigationMobile/NavigationItemMobile.test.js
@@ -4,28 +4,129 @@ import { shallow } from "enzyme";
 import NavigationItemMobile from "./NavigationItemMobile";
 
 const mockTag = {
-  _id: "123",
-  name: "Shop",
-  slug: "shop"
-};
-
-const mockTagWithSubTags = {
-  _id: "123",
-  name: "Shop",
-  slug: "shop",
+  _id: "cmVhY3Rpb24vdGFnOjJFcEdxYUx2TDRmNkFYRVhC",
+  position: null,
+  slug: "tag-slug",
+  name: "Tag Name",
   subTags: {
     edges: []
   }
 };
 
-test("basic snapshot", () => {
+const mockTagWithSubTags = {
+  _id: "cmVhY3Rpb24vdGFnOjVXZE5LYXprejZ5TWdrcGY5",
+  position: null,
+  slug: "tag-slug",
+  name: "Tag Name",
+  subTags: {
+    edges: [
+      {
+        node: {
+          _id: "cmVhY3Rpb24vdGFnOlRFdjdSV1pDTHl1d1dmOWs2",
+          position: null,
+          slug: "sub-tag-slug",
+          name: "Sub Tag Name",
+          subTags: {
+            edges: []
+          }
+        }
+      },
+      {
+        node: {
+          _id: "cmVhY3Rpb24vdGFnOmJ2MmVEbzV3V3lvNUdNMlN3",
+          position: null,
+          slug: "sub-tag-slug",
+          name: "Sub Tag name",
+          subTags: {
+            edges: []
+          }
+        }
+      }
+    ]
+  }
+};
+
+const mockTagsWithMoreSubTags = {
+  _id: "cmVhY3Rpb24vdGFnOkdyZ1dkYXVwOVB1ZlBYNEp6",
+  position: null,
+  slug: "tag-slug",
+  name: "Tag Name",
+  subTags: {
+    edges: [
+      {
+        node: {
+          _id: "cmVhY3Rpb24vdGFnOjVXZE5LYXprejZ5TWdrcGY5",
+          position: null,
+          slug: "sub-tag-slug",
+          name: "Sub Tag Slug",
+          subTags: {
+            edges: [
+              {
+                node: {
+                  _id: "cmVhY3Rpb24vdGFnOlRFdjdSV1pDTHl1d1dmOWs2",
+                  position: null,
+                  slug: "sub-sub-tag-slug",
+                  name: "Sub Sub Tag name"
+                }
+              },
+              {
+                node: {
+                  _id: "cmVhY3Rpb24vdGFnOmJ2MmVEbzV3V3lvNUdNMlN3",
+                  position: null,
+                  slug: "sub-sub-tag-slug",
+                  name: "Sub Sub Tag Name"
+                }
+              }
+            ]
+          }
+        }
+      },
+      {
+        node: {
+          _id: "cmVhY3Rpb24vdGFnOkZHNnRLdkNvZGVId21Qb05p",
+          position: null,
+          slug: "sub-tag-slug",
+          name: "Sub Tag Name",
+          subTags: {
+            edges: [
+              {
+                node: {
+                  _id: "cmVhY3Rpb24vdGFnOjREelpZdG1ydTVZaXFYSHR3",
+                  position: null,
+                  slug: "sub-sub-tag-slug",
+                  name: "Sub Sub Tag Name"
+                }
+              },
+              {
+                node: {
+                  _id: "cmVhY3Rpb24vdGFnOjg3aW1wYTNKUDVhWVFKalJE",
+                  position: null,
+                  slug: "sub-sub-tag-slug",
+                  name: "Sub Sub Tag Name"
+                }
+              }
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
+
+test("renders with 1 level of tags", () => {
   const component = renderer.create(shallow(<NavigationItemMobile navItem={mockTag} />).get(3));
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
-test("snapshot with related items", () => {
+test("renders with 2 levels of tags", () => {
   const component = renderer.create(<NavigationItemMobile navItem={mockTagWithSubTags} />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("renders with 3 levels of tags", () => {
+  const component = renderer.create(<NavigationItemMobile navItem={mockTagsWithMoreSubTags} />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
