1e5846f1a66467243f993d4edbc17f1c3c1b9ecd
Mike Murray <hellomikemurray@gmail.com>
2018-04-20 11:30:21 -0700

    test: add additional snapshot tests

--- src/components/ProductDetailInfo/ProductDetailInfo.test.js
+++ src/components/ProductDetailInfo/ProductDetailInfo.test.js
@@ -13,3 +13,50 @@ test("basic snapshot", () => {
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+test("snapshot with price only", () => {
+  const component = renderer.create((
+    <ProductDetailInfo
+      priceRange="$19.99"
+    />
+  ));
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("snapshot with vendor only", () => {
+  const component = renderer.create((
+    <ProductDetailInfo
+      vendor="Vendor"
+    />
+  ));
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("snapshot with description only", () => {
+  const component = renderer.create((
+    <ProductDetailInfo
+      description="Description"
+    />
+  ));
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("snapshot with price and description", () => {
+  const component = renderer.create((
+    <ProductDetailInfo
+      priceRange="$19.99"
+      description="Description"
+    />
+  ));
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("snapshot with no props", () => {
+  const component = renderer.create(<ProductDetailInfo />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
