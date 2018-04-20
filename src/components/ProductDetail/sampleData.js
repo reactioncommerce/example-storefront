1dacdb282f66c8ad0e447f4f05ca0e06bc7b8e3a
Mike Murray <hellomikemurray@gmail.com>
2018-04-20 08:48:05 -0700

    test: add more fields to sample data

--- src/components/ProductDetail/sampleData.js
+++ src/components/ProductDetail/sampleData.js
@@ -1,5 +1,10 @@
 // Sample Catalog Product Data
 export default {
   title: "Title",
-  pageTitle: "SubTitle"
+  pageTitle: "SubTitle",
+  description: "Description",
+  vendor: "Vendor",
+  price: {
+    range: "$19.99"
+  }
 };
