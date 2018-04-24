76d776eb5158cf2843b6bb26a07b6b46af1123f9
Mike Murray <hellomikemurray@gmail.com>
2018-04-24 11:58:56 -0700

    test: add media to mock data

--- src/components/ProductDetail/__mocks__/productData.mock.js
+++ src/components/ProductDetail/__mocks__/productData.mock.js
@@ -6,5 +6,33 @@ export default {
   vendor: "Vendor",
   price: {
     range: "$19.99"
-  }
+  },
+  media: [
+    {
+      toGrid: 1,
+      priority: 0,
+      productId: "cmVhY3Rpb24vcHJvZHVjdDpCQ1RNWjZIVHhGU3BwSkVTaw==",
+      variantId: "cmVhY3Rpb24vcHJvZHVjdDo2cWlxUHdCa2VKZHRkUWM0Rw==",
+      URLs: {
+        large: "/assets/files/Media/P3MkhDWadHoDNsFPE/large/I0DhYYC.jpg",
+        medium: "/assets/files/Media/P3MkhDWadHoDNsFPE/medium/I0DhYYC.jpg",
+        original: "/assets/files/Media/P3MkhDWadHoDNsFPE/image/I0DhYYC.jpg",
+        small: "/assets/files/Media/P3MkhDWadHoDNsFPE/small/I0DhYYC.png",
+        thumbnail: "/assets/files/Media/P3MkhDWadHoDNsFPE/thumbnail/I0DhYYC.png"
+      }
+    },
+    {
+      toGrid: 1,
+      priority: 0,
+      productId: "cmVhY3Rpb24vcHJvZHVjdDpCQ1RNWjZIVHhGU3BwSkVTaw==",
+      variantId: "cmVhY3Rpb24vcHJvZHVjdDo2cWlxUHdCa2VKZHRkUWM0Rw==",
+      URLs: {
+        large: "/assets/files/Media/jYkxma8zveTG9f9Q9/large/cB4Ao8V.jpg",
+        medium: "/assets/files/Media/jYkxma8zveTG9f9Q9/medium/cB4Ao8V.jpg",
+        original: "/assets/files/Media/jYkxma8zveTG9f9Q9/image/cB4Ao8V.jpg",
+        small: "/assets/files/Media/jYkxma8zveTG9f9Q9/small/cB4Ao8V.png",
+        thumbnail: "/assets/files/Media/jYkxma8zveTG9f9Q9/thumbnail/cB4Ao8V.png"
+      }
+    }
+  ]
 };
