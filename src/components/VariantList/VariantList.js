39d5ae10d52810da9801282789dc7045cd2a3191
Will Lopez <will.lopez77@gmail.com>
2018-04-24 14:47:37 -0700

    Use mobx to track selected variant

--- src/components/VariantList/VariantList.js
+++ src/components/VariantList/VariantList.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "material-ui/styles";
-import nr from "next/router";
+import { observable, action, computed } from "mobx";
+import { observer } from "mobx-react";
 
 import { Router } from "routes";
 import VariantItem from "components/VariantItem";
@@ -21,19 +22,25 @@ const styles = () => ({
 });
 
 @withStyles(styles)
+@observer
 class VariantList extends Component {
-  // Default to select first variant
-  state = { activeVariantId: variants[0].node.product._id }
+  @observable _selectedVariant = variants[0].node.product._id;
 
+  @action
   handleClick = (variant) => {
+    this._selectedVariant = variant._id;
     Router.pushRoute("product", { productSlug: variant.slug, variantId: variant._id });
-    this.setState({ activeVariantId: variant._id });
+  }
+
+  @computed
+  get selectedVariant() {
+    return this._selectedVariant;
   }
 
   renderVariant = (variant) => {
     const { classes: { variantItem } } = this.props;
     const { node: { product } } = variant;
-    const active = (this.state.activeVariantId === product._id);
+    const active = (this.selectedVariant === product._id);
 
     return (
       <li className={variantItem} key={product._id}>
