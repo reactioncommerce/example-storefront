642da9c18f9bac2a69e20e899a1c19248c9f9487
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-04-25 09:51:14 -0700

    add react helmet to product grid index with shop meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { observer, inject } from "mobx-react";
+import Helmet from "react-helmet";
 
 import withData from "lib/apollo/withData";
 import withCatalogItems from "containers/catalog/withCatalogItems";
@@ -13,16 +14,30 @@ import ProductGrid from "components/ProductGrid";
 @withShop
 @withCatalogItems
 @withRoot
+@inject("shop")
 @inject("uiStore")
 @observer
+
 class Shop extends Component {
   static propTypes = {
     catalogItems: PropTypes.array.isRequired
   };
 
+  renderHelmet() {
+    const { shop } = this.props;
+
+    return (
+      <Helmet>
+        <title>{shop.name}</title>
+        <meta name="description" content={shop.description} />
+      </Helmet>
+    );
+  }
+
   render() {
     return (
       <Layout title="Reaction Shop">
+        {this.renderHelmet()}
         <ProductGrid catalogItems={this.props.catalogItems}/>
       </Layout>
     );
