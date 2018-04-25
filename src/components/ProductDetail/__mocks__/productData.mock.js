af7f85207c68ad31570965ad6477211d681d5ec3
Mike Murray <hellomikemurray@gmail.com>
2018-04-25 08:38:24 -0700

    test: update mock data

--- src/components/ProductDetail/__mocks__/productData.mock.js
+++ src/components/ProductDetail/__mocks__/productData.mock.js
@@ -33,6 +33,84 @@ export default {
         small: "/assets/files/Media/jYkxma8zveTG9f9Q9/small/cB4Ao8V.png",
         thumbnail: "/assets/files/Media/jYkxma8zveTG9f9Q9/thumbnail/cB4Ao8V.png"
       }
+    },
+    {
+      toGrid: 1,
+      priority: 0,
+      productId: "cmVhY3Rpb24vcHJvZHVjdDpCQ1RNWjZIVHhGU3BwSkVTaw==",
+      variantId: "cmVhY3Rpb24vcHJvZHVjdDpTTXI0cmhERm5ZdkZNdERUWA==",
+      URLs: {
+        large: "/assets/files/Media/XnuHto5KgFRQH4k7d/large/eMVBUmD.jpg",
+        medium: "/assets/files/Media/XnuHto5KgFRQH4k7d/medium/eMVBUmD.jpg",
+        original: "/assets/files/Media/XnuHto5KgFRQH4k7d/image/eMVBUmD.jpg",
+        small: "/assets/files/Media/XnuHto5KgFRQH4k7d/small/eMVBUmD.png",
+        thumbnail: "/assets/files/Media/XnuHto5KgFRQH4k7d/thumbnail/eMVBUmD.png"
+      }
+    },
+    {
+      toGrid: 1,
+      priority: 0,
+      productId: "cmVhY3Rpb24vcHJvZHVjdDpCQ1RNWjZIVHhGU3BwSkVTaw==",
+      variantId: "cmVhY3Rpb24vcHJvZHVjdDpDSm9SQm05dlJyb3JjOW14Wg==",
+      URLs: {
+        large: "/assets/files/Media/4zNFkNwCnErxewpLF/large/Mj460Zo.jpg",
+        medium: "/assets/files/Media/4zNFkNwCnErxewpLF/medium/Mj460Zo.jpg",
+        original: "/assets/files/Media/4zNFkNwCnErxewpLF/image/Mj460Zo.jpg",
+        small: "/assets/files/Media/4zNFkNwCnErxewpLF/small/Mj460Zo.png",
+        thumbnail: "/assets/files/Media/4zNFkNwCnErxewpLF/thumbnail/Mj460Zo.png"
+      }
+    },
+    {
+      toGrid: 1,
+      priority: 2,
+      productId: "cmVhY3Rpb24vcHJvZHVjdDpCQ1RNWjZIVHhGU3BwSkVTaw==",
+      variantId: "cmVhY3Rpb24vcHJvZHVjdDo2cWlxUHdCa2VKZHRkUWM0Rw==",
+      URLs: {
+        large: "/assets/files/Media/4nvNZibEbtfnTBaSx/large/belle-ALT1.jpg",
+        medium: "/assets/files/Media/4nvNZibEbtfnTBaSx/medium/belle-ALT1.jpg",
+        original: "/assets/files/Media/4nvNZibEbtfnTBaSx/image/belle-ALT1.jpg",
+        small: "/assets/files/Media/4nvNZibEbtfnTBaSx/small/belle-ALT1.jpg",
+        thumbnail: "/assets/files/Media/4nvNZibEbtfnTBaSx/thumbnail/belle-ALT1.jpg"
+      }
+    },
+    {
+      toGrid: 1,
+      priority: 3,
+      productId: "cmVhY3Rpb24vcHJvZHVjdDpCQ1RNWjZIVHhGU3BwSkVTaw==",
+      variantId: "cmVhY3Rpb24vcHJvZHVjdDo2cWlxUHdCa2VKZHRkUWM0Rw==",
+      URLs: {
+        large: "/assets/files/Media/yfAMghTtzbhPqyqLk/large/belle-ALT2.jpg",
+        medium: "/assets/files/Media/yfAMghTtzbhPqyqLk/medium/belle-ALT2.jpg",
+        original: "/assets/files/Media/yfAMghTtzbhPqyqLk/image/belle-ALT2.jpg",
+        small: "/assets/files/Media/yfAMghTtzbhPqyqLk/small/belle-ALT2.jpg",
+        thumbnail: "/assets/files/Media/yfAMghTtzbhPqyqLk/thumbnail/belle-ALT2.jpg"
+      }
+    },
+    {
+      toGrid: 1,
+      priority: 4,
+      productId: "cmVhY3Rpb24vcHJvZHVjdDpCQ1RNWjZIVHhGU3BwSkVTaw==",
+      variantId: "cmVhY3Rpb24vcHJvZHVjdDo2cWlxUHdCa2VKZHRkUWM0Rw==",
+      URLs: {
+        large: "/assets/files/Media/9T94xPRnYGDacJdJ5/large/belle-ALT3.jpg",
+        medium: "/assets/files/Media/9T94xPRnYGDacJdJ5/medium/belle-ALT3.jpg",
+        original: "/assets/files/Media/9T94xPRnYGDacJdJ5/image/belle-ALT3.jpg",
+        small: "/assets/files/Media/9T94xPRnYGDacJdJ5/small/belle-ALT3.jpg",
+        thumbnail: "/assets/files/Media/9T94xPRnYGDacJdJ5/thumbnail/belle-ALT3.jpg"
+      }
+    },
+    {
+      toGrid: 1,
+      priority: 5,
+      productId: "cmVhY3Rpb24vcHJvZHVjdDpCQ1RNWjZIVHhGU3BwSkVTaw==",
+      variantId: "cmVhY3Rpb24vcHJvZHVjdDo2cWlxUHdCa2VKZHRkUWM0Rw==",
+      URLs: {
+        large: "/assets/files/Media/JhcDN3YKTEnNn8p3J/large/belle-HERO.jpg",
+        medium: "/assets/files/Media/JhcDN3YKTEnNn8p3J/medium/belle-HERO.jpg",
+        original: "/assets/files/Media/JhcDN3YKTEnNn8p3J/image/belle-HERO.jpg",
+        small: "/assets/files/Media/JhcDN3YKTEnNn8p3J/small/belle-HERO.jpg",
+        thumbnail: "/assets/files/Media/JhcDN3YKTEnNn8p3J/thumbnail/belle-HERO.jpg"
+      }
     }
   ]
 };
