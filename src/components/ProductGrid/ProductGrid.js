6485284c1f250651a73f178cbc2cb6d734e23192
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-04-25 09:52:09 -0700

    revert productGrid.js changes

--- src/components/ProductGrid/ProductGrid.js
+++ src/components/ProductGrid/ProductGrid.js
@@ -1,38 +1,18 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Grid from "material-ui/Grid";
-import Helmet from "react-helmet";
-import { inject, observer } from "mobx-react";
 
 import ProductItem from "components/ProductItem";
 
-// TODO: get real products from server
-import { tempProducts } from "./tempProducts";
-
-@inject("shop")
-@observer
 class ProductGrid extends Component {
   static propTypes = {
     catalogItems: PropTypes.arrayOf(PropTypes.object)
   };
 
-  renderHelmet() {
-    const { shop } = this.props;
-
-    // If we are on the default Product Grid, use default shop info
-    return (
-      <Helmet>
-        <title>{shop.name}</title>
-        <meta name="description" content={shop.description} />
-      </Helmet>
-    );
-  }
-
   renderProduct(edge) {
     const { node: { product, positions } } = edge;
     const weight = (positions.length) ? positions[0].displayWeight : 0;
     const { _id } = product;
-
     const gridItemSize = {
       0: {
         xs: 12,
@@ -66,9 +46,8 @@ class ProductGrid extends Component {
 
     return (
       <section>
-        {this.renderHelmet()}
         <Grid container spacing={24}>
-          {(catalogItems && catalogItems.length) ? catalogItems.map(this.renderProduct) : null }
+          {(catalogItems && catalogItems.length) ? catalogItems.map(this.renderProduct) : null}
         </Grid>
       </section>
     );
