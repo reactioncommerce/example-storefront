59b652d63fa30f1b03227c3e5f20ca1c6fc382e2
Will Lopez <will.lopez77@gmail.com>
2018-04-30 12:14:52 -0700

    Update tests snapshots.

--- src/components/Divider/Divider.test.js
+++ src/components/Divider/Divider.test.js
@@ -1,10 +1,14 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { MuiThemeProvider } from "material-ui/styles";
+import theme from "lib/theme/reactionTheme";
 import Divider from "./Divider";
 
 test("basic snapshot", () => {
   const component = renderer.create((
-    <Divider />
+    <MuiThemeProvider theme={theme}>
+      <Divider />
+    </MuiThemeProvider>
   ));
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
