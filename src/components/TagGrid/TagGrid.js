247e03f9053d1587dba6ba51ebb346c50e5144f7
Eric Dobbertin <eric@dairystatedesigns.com>
2018-04-30 07:34:32 -0500

    fix: use proper key for React array map

--- src/components/TagGrid/TagGrid.js
+++ src/components/TagGrid/TagGrid.js
@@ -52,9 +52,9 @@ export default class TagGrid extends Component {
         <Typography className={classes.title} variant="title">{"Tags"}</Typography>
         <Grid container spacing={theme.spacing.unit}>
           {tags.map(({ node: tag }, index) => (
-            <Grid item>
+            <Grid item key={tag.slug}>
               <Link route={`/tag/${tag.slug}`}>
-                <Chip className={classes.chip} key={index} label={tag.name} />
+                <Chip className={classes.chip} label={tag.name} />
               </Link>
             </Grid>
           ))}
