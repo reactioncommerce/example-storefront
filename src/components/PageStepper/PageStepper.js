e44ddcb21c429a503f5bb99798d493761864b10b
Mike Murray <hellomikemurray@gmail.com>
2018-04-30 18:31:26 -0700

    refactor: adjust styling and layout

--- src/components/PageStepper/PageStepper.js
+++ src/components/PageStepper/PageStepper.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Button from "material-ui/Button";
+import Grid from "material-ui/Grid";
+import withStyles from "material-ui/styles/withStyles";
 
+const styles = (theme) => ({
+  root: {
+    margin: theme.spacing.unit * 2
+  }
+});
+
+@withStyles(styles, { withTheme: true })
 export default class ProductGrid extends Component {
   static propTypes = {
+    classes: PropTypes.object,
     pageInfo: PropTypes.shape({
       startCursor: PropTypes.string,
       endCursor: PropTypes.string,
@@ -11,21 +21,28 @@ export default class ProductGrid extends Component {
       hasPreviousPage: PropTypes.bool,
       loadNextPage: PropTypes.func,
       loadPreviousPage: PropTypes.func
-    })
+    }),
+    theme: PropTypes.object
   };
 
   render() {
-    const { pageInfo } = this.props;
+    const { classes, pageInfo, theme } = this.props;
 
     return (
-      <div>
+      <Grid className={classes.root} container justify="center" spacing={theme.spacing.unit * 2}>
         {pageInfo.hasPreviousPage &&
-          <Button onClick={pageInfo.loadPreviousPage}>Previous</Button>
+          <Grid item>
+            <Button onClick={pageInfo.loadPreviousPage} variant="raised">Previous</Button>
+          </Grid>
         }
         {pageInfo.hasNextPage &&
-          <Button onClick={pageInfo.loadNextPage}>Next</Button>
+          <Grid item>
+            <Button onClick={pageInfo.loadNextPage} variant="raised">
+              Next
+            </Button>
+          </Grid>
         }
-      </div>
+      </Grid>
     );
   }
 }
