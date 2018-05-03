732729bd870edb5f90726c64e26df8ae2211ece7
Will Lopez <will.lopez77@gmail.com>
2018-05-03 11:44:38 -0700

    Add theme provider to ProductGrid

--- src/components/ProductGrid/ProductGrid.test.js
+++ src/components/ProductGrid/ProductGrid.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import { Provider } from "mobx-react";
+import { MuiThemeProvider } from "material-ui/styles";
+import theme from "lib/theme/reactionTheme";
 import ProductGrid from "./ProductGrid";
 import products from "./__mocks__/products.mock";
 
@@ -14,9 +16,11 @@ const uiStore = {
 
 test("basic snapshot", () => {
   const component = renderer.create((
-    <Provider uiStore={uiStore}>
-      <ProductGrid catalogItems={products} primaryShopId="123" />
-    </Provider>
+    <MuiThemeProvider theme={theme}>
+      <Provider uiStore={uiStore}>
+        <ProductGrid catalogItems={products} primaryShopId="123" />
+      </Provider>
+    </MuiThemeProvider>
   ));
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
