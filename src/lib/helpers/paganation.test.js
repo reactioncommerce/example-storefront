2eb4ec2832ed92df95a10dbf3d8c246a5c2eef38
Mike Murray <hellomikemurray@gmail.com>
2018-05-03 14:00:09 -0700

    test: add paganation test

---
+++ src/lib/helpers/paganation.test.js
@@ -0,0 +1,115 @@
+import {
+  loadNextPage,
+  loadPreviousPage,
+  paganation,
+  paganationVariablesFromUrlParams
+} from "./paganation";
+
+const args = {
+  fetchMore: jest.fn(({ updateQuery }) => {
+    updateQuery(args.data, {
+      fetchMoreResult: {
+        test: {
+          edges: []
+        }
+      }
+    });
+  }),
+  routingStore: {
+    setSearch: jest.fn()
+  },
+  queryName: "test",
+  limit: 5,
+  data: {
+    test: {
+      pageInfo: {
+        hasNextPage: true,
+        hasPrevioudPage: false,
+        startCursor: "1",
+        endCursor: "5"
+      },
+      edges: [
+        { cursor: "1", node: { title: "1" } },
+        { cursor: "2", node: { title: "2" } },
+        { cursor: "3", node: { title: "3" } },
+        { cursor: "4", node: { title: "4" } },
+        { cursor: "5", node: { title: "5" } }
+      ]
+    }
+  }
+};
+
+test("paganation helper creates helper function props", () => {
+  const props = paganation(args);
+
+  expect(typeof props.loadNextPage).toBe("function");
+  expect(typeof props.loadPreviousPage).toBe("function");
+});
+
+
+test("loadNextPage helper creates helper function", () => {
+  const func = loadNextPage(args);
+
+  // Call function to ensure methods it calls, are called
+  func();
+  expect(args.fetchMore).toHaveBeenCalled();
+  expect(args.routingStore.setSearch).toHaveBeenCalled();
+});
+
+test("loadPreviousPage helper creates helper function", () => {
+  const func = loadPreviousPage(args);
+
+  // Call function to ensure methods it calls, are called
+  func();
+  expect(args.fetchMore).toHaveBeenCalled();
+  expect(args.routingStore.setSearch).toHaveBeenCalled();
+});
+
+test("paganation variables from url params, (first, after)", () => {
+  const variables = paganationVariablesFromUrlParams({
+    first: 2,
+    after: "6"
+  });
+
+  expect(variables).toEqual({
+    first: 2,
+    after: "6"
+  });
+});
+
+test("paganation variables from url params, (before, last)", () => {
+  const variables = paganationVariablesFromUrlParams({
+    last: 10,
+    before: "3"
+  });
+
+  expect(variables).toEqual({
+    last: 10,
+    before: "3"
+  });
+});
+
+test("paganation variables from url params (first, after) supercede (last, before)", () => {
+  const variables = paganationVariablesFromUrlParams({
+    first: 2,
+    after: "6",
+    last: 10,
+    before: "3"
+  });
+
+  expect(variables).toEqual({
+    first: 2,
+    after: "6"
+  });
+});
+
+test("paganation variables from url params with defaultPageLimit", () => {
+  const variables = paganationVariablesFromUrlParams({
+    after: "6"
+  }, { defaultPageLimit: 10 });
+
+  expect(variables).toEqual({
+    first: 10,
+    after: "6"
+  });
+});
