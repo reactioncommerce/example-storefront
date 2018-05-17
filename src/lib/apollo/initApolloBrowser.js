d37d8edf2a17d1f4d7983eb42fc982a9ff0da198
Jeremy Shimko <jeremy.shimko@gmail.com>
2018-05-17 10:40:17 -0400

    proper log level

--- src/lib/apollo/initApolloBrowser.js
+++ src/lib/apollo/initApolloBrowser.js
@@ -18,13 +18,13 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors) {
     graphQLErrors.forEach(({ message, locations, path }) => {
       // eslint-disable-next-line no-console
-      console.log(`[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(locations)}, Path: ${JSON.stringify(path)}`);
+      console.error(`[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(locations)}, Path: ${JSON.stringify(path)}`);
     });
   }
 
   if (networkError) {
     // eslint-disable-next-line no-console
-    console.log(`[Network error]: ${networkError}`);
+    console.error(`[Network error]: ${networkError}`);
   }
 });
 
