f7746c7cd729e023b0d7653d5b660476aac62a98
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-05-22 11:57:45 -0700

    add ProductGridHero component styles

---
+++ src/components/ProductGridHero/styles.js
@@ -0,0 +1,12 @@
+export default (theme) => ({
+  heroImg: {
+    width: "100%",
+    height: "325px",
+    objectFit: "cover"
+  },
+  productGridContainer: {
+    maxWidth: theme.grid.productGridMaxWidth,
+    marginLeft: "auto",
+    marginRight: "auto"
+  }
+});
