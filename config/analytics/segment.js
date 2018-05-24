b337764dcccc7dda81c10e7c6ad6d43ed2eeb7e4
Mike Murray <hellomikemurray@gmail.com>
2018-05-24 15:20:32 -0700

    feat: add segment analytics provider

---
+++ config/analytics/segment.js
@@ -0,0 +1,32 @@
+import getConfig from "next/config";
+import * as snippet from "@segment/snippet";
+
+/**
+ * Dispatch tracking data to segment
+ * @name dispatch
+ * @ignore
+ * @param {Object} data Arguments supplied by tracking library
+ * @param {String} data.action String value used as the first param to analytics.track()
+ * @returns {undefined} No Return
+ */
+export function dispatch({ action, ...trackingData }) {
+  window && window.analytics.track(action, trackingData);
+}
+
+/**
+ * Render string script
+ * @returns {String} String script to be included in the document head
+ */
+export function renderScript() {
+  const { publicRuntimeConfig } = getConfig();
+  const opts = {
+    apiKey: publicRuntimeConfig.segmentAnalyticsWriteKey,
+    page: true // Set this to `false` if you want to manually fire `analytics.page()` from within your pages.
+  };
+
+  if (process.env.NODE_ENV === "development") {
+    return snippet.max(opts);
+  }
+
+  return snippet.min(opts);
+}
