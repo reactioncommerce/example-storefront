bfad26957e6e115683c6e724f337b07671f94691
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-05-23 21:20:40 -0700

    add withTag wrapper

---
+++ src/containers/tags/withTag.js
@@ -0,0 +1,49 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Query } from "react-apollo";
+import { computed, observable, toJS } from "mobx";
+import { observer } from "mobx-react";
+import tagQuery from "./tag.gql";
+
+/**
+ * withTag higher order query component for fetching tag information
+ * @name WithTag
+ * @param {React.Component} Component to decorate
+ * @returns {React.Component} - Component with `tag` prop
+ */
+export default (Component) => (
+  @observer
+  class WithTag extends React.Component {
+    static propTypes = {
+      /**
+       * tagId used to obtain tag info
+       */
+      tag: PropTypes.string.isRequired
+    }
+    @observable _data = {}
+
+    @computed get data() { return toJS(this._data); }
+    set data(value) { this._data = value; }
+
+
+    render() {
+      const { tag } = this.props;
+
+      return (
+        <Query query={tagQuery} variables={{ slugOrId: tag }}>
+          {({ loading, error, data }) => {
+            if (loading || error) return null;
+            if (!data || !data.tag) return null;
+
+            return (
+              <Component
+                {...this.props}
+                tag={data.tag}
+              />
+            );
+          }}
+        </Query>
+      );
+    }
+  }
+);
