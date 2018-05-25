cb5c02e274e75e94d71c59a4495c8beca67d1fca
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-05-25 10:56:10 -0700

    new test added

--- src/components/ProductGridHero/ProductGridHero.test.js
+++ src/components/ProductGridHero/ProductGridHero.test.js
@@ -13,11 +13,51 @@ const uiStore = {
   }
 };
 
-test("basic snapshot", () => {
+const tagWithMedia = {
+  name: "TagWithMedia",
+  _id: "rubhpcpSzrgZuBCEM",
+  slug: "tag-with-media",
+  isTopLevel: true,
+  updatedAt: "2018-05-25 17:48:29.745Z",
+  createdAt: "2018-05-25 17:48:29.745Z",
+  isDeleted: false,
+  isVisible: true,
+  groups: [],
+  shopId: "J8Bhq3uTtdgwZx3rz",
+  heroMediaUrl: "https://www.reactioncommerce.com/assets/careers-images-2.jpg"
+};
+
+const tagWithoutMedia = {
+  name: "TagWithoutMedia",
+  _id: "rubhpcpSzrgZuBCEM",
+  slug: "tag-withpoiut-media",
+  isTopLevel: true,
+  updatedAt: "2018-05-25 17:48:29.745Z",
+  createdAt: "2018-05-25 17:48:29.745Z",
+  isDeleted: false,
+  isVisible: true,
+  groups: [],
+  shopId: "J8Bhq3uTtdgwZx3rz"
+};
+
+
+test("snapshot of tagWithMedia", () => {
   const component = renderer.create((
     <MuiThemeProvider theme={theme}>
       <Provider uiStore={uiStore}>
-        <ProductGridHero />
+        <ProductGridHero tag={tagWithMedia} />
+      </Provider>
+    </MuiThemeProvider>
+  ));
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("snapshop of tagWithoutMedia", () => {
+  const component = renderer.create((
+    <MuiThemeProvider theme={theme}>
+      <Provider uiStore={uiStore}>
+        <ProductGridHero tag={tagWithoutMedia} />
       </Provider>
     </MuiThemeProvider>
   ));
