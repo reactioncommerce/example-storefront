a7db70a56e52662c93706fd8eef377a6e62ad4e4
Mike Murray <hellomikemurray@gmail.com>
2018-06-01 14:46:53 -0700

    refactor: remove action

--- src/lib/tracking/utils/getVariantTrackingData.js
+++ src/lib/tracking/utils/getVariantTrackingData.js
@@ -1,12 +1,11 @@
 /**
- * Transform a variant object into a partial representation of the segment Proeduct Viewed schema
+ * Transform a variant object into a partial representation of the Segment product schema
  * @name getVariantTrackingData
  * @param {Object} variant Project variant
  * @returns {Object} Data sutable for trackign a variant
  */
 export default function getVariantTrackingData(variant) {
   return {
-    action: "Product Viewed",
     variant: variant.variantId,
     price: variant.price,
     quantity: 1,
