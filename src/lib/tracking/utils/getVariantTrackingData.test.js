6e1ea74c68b947fcbf419c97b80f4f8a5d016c06
Mike Murray <hellomikemurray@gmail.com>
2018-06-01 14:30:32 -0700

    test:  add test for getVariantTrackingData

---
+++ src/lib/tracking/utils/getVariantTrackingData.test.js
@@ -0,0 +1,23 @@
+import getVariantTrackingData from "./getVariantTrackingData";
+
+const variant = {
+  variantId: "1234",
+  title: "Men's Lightweigth Synchilla",
+  index: 0,
+  price: 19.99
+};
+
+test("getVariantTrackingData should return partial data for tracking with segment Product schema", () => {
+  const data = getVariantTrackingData(variant);
+
+  const result = {
+    action: "Product Viewed",
+    variant: "1234",
+    price: 19.99,
+    quantity: 1,
+    position: 0,
+    value: 19.99
+  };
+
+  expect(data).toEqual(result);
+});
