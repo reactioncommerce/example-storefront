656ddaa800598d0fa5760d1ceccced3299c27aeb
Mike Murray <hellomikemurray@gmail.com>
2018-06-05 15:56:11 -0700

    refactor: use getProductListTrackingData to construct data
    Simplify HOC implementation

--- src/lib/tracking/trackProductListViewed.js
+++ src/lib/tracking/trackProductListViewed.js
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
-import React from "react";
 import track from "./track";
+import getProductListTrackingData from "./utils/getProductListTrackingData";
 
 /**
  * trackProductListViewed higher tracks a list of viewed products
@@ -8,34 +8,9 @@ import track from "./track";
  * @param {Object} options options to supply to tracking HOC
  * @returns {React.Component} - component
  */
-export default (options) => (Component) => {
-  const component = class trackProductListViewed extends React.Component {
-    render() {
-      return (
-        <Component {...this.props} />
-      );
-    }
-  };
-
-  return track(({ catalogItems }) => {
-    let products = [];
-
-    if (Array.isArray(catalogItems)) {
-      // TODO: conform to segment.io schema
-      // Issue: https://github.com/reactioncommerce/reaction-next-starterkit/issues/56
-      // https://segment.com/docs/spec/ecommerce/v2/#product-list-viewed
-      products = catalogItems.map((catalogItem) => ({
-        product_id: catalogItem.node._id
-      }));
-    }
-
-    return {
-      action: "Product List Viewed",
-      list_id: "",
-      category: "",
-      products
-    };
-  }, {
-    ...options
-  })(component);
-};
+export default (options) => track(({ tag, catalogItems }) => {
+  const products = (Array.isArray(catalogItems) && catalogItems.map((catalogItem) => catalogItem.node)) || undefined;
+  return getProductListTrackingData({ tag, products });
+}, {
+  ...options
+});
