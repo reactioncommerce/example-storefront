b6d49065abc07c5d91e941b3f53151ac2a9af4c7
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-06-06 15:43:04 -0700

    introduce logic to ProductItemBadges

--- src/components/ProductItemBadges/ProductItemBadges.js
+++ src/components/ProductItemBadges/ProductItemBadges.js
@@ -1,56 +1,47 @@
 import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 import { withStyles } from "@material-ui/core/styles";
 import { inject, observer } from "mobx-react";
 import Badge from "components/Badge";
-import { inventoryStatus, isProductLowQuantity, INVENTORY_STATUS, priceByCurrencyCode } from "lib/utils";
-// import { styles } from "./styles";
-
+import { inventoryStatus, isProductBestSeller, isProductLowQuantity, INVENTORY_STATUS } from "lib/utils";
 
 const styles = (theme) => ({
   badgeOverlay: {
     position: "relative"
   },
-  badge: {
-    ...theme.typography.caption,
-    borderRadius: 4,
-    height: "auto",
-    fontSize: "0.7rem",
-    paddingBottom: theme.spacing.unithalf,
-    paddingLeft: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingTop: theme.spacing.unithalf,
-    position: "absolute",
-    zIndex: 100
-  },
-  labelStyle: {
-    fontWeight: theme.typography.fontWeightBold,
-    position: "relative",
-    whiteSpace: "nowrap",
-    padding: 0
+  faded: {
+    opacity: "0.5"
   },
   status: {
     color: theme.palette.primary.contrastText,
     left: theme.spacing.unit,
     top: theme.spacing.unit
   },
-  soldOut: {
-    backgroundColor: theme.palette.secondary.main
-  },
   backorder: {
-    backgroundColor: theme.palette.secondary.dark
-  },
-  sale: {
-    backgroundColor: theme.palette.error.main
+    backgroundColor: theme.palette.reaction.coolGrey
   },
   bestseller: {
-    backgroundColor: theme.palette.reaction.bestseller
+    backgroundColor: theme.palette.reaction.badges.bestseller
+  },
+  lowInventory: {
+    backgroundColor: theme.palette.reaction.coolGrey
+  },
+  sale: {
+    backgroundColor: theme.palette.reaction.badges.sale
+  },
+  soldOut: {
+    backgroundColor: theme.palette.reaction.coolGrey
   },
   warning: {
     backgroundColor: "transparent",
     color: theme.palette.secondary.main
   },
   alignRight: {
+    right: 0
+  },
+  secondaryBadge: {
+    color: theme.palette.reaction.coolGrey,
     right: 0
   }
 });
@@ -70,23 +61,80 @@ class ProductItemBadges extends Component {
     classes: {}
   };
 
-  renderBadge() {
-    const { product } = this.props;
-    const status = inventoryStatus(product);
+  renderBadge = () => {
+    const { classes, product } = this.props;
+    const status = inventoryStatus(product) || {};
 
+    const badgeClasses = classNames({
+      [classes.status]: true,
+      [classes.backorder]: status.type === INVENTORY_STATUS.BACKORDER,
+      [classes.bestseller]: status.type === INVENTORY_STATUS.BESTSELLER,
+      [classes.lowInventory]: status.type === INVENTORY_STATUS.LOW_QUANTITY,
+      [classes.sale]: status.type === INVENTORY_STATUS.SALE,
+      [classes.soldOut]: status.type === INVENTORY_STATUS.SOLD_OUT
+    });
+
+    // If status is "BACKORDER" or "SOLD_OUT", only show primary badge
+    if (status.type === "BACKORDER" || status.type === "SOLD_OUT") {
+      return (
+        <Fragment>
+          {status && <Badge badgeClasses={badgeClasses} label={status.label} />}
+        </Fragment>
+      );
+    }
     return (
       <Fragment>
-        {status && <Badge type={status.type} label={status.label} />}
-        {isProductLowQuantity(product) && <Badge type={INVENTORY_STATUS.LOW_QUANTITY} label="Low Inventory" />}
+        {status && <Badge badgeClasses={badgeClasses} label={status.label} />}
+        {this.renderSecondaryBadge(status.type)}
       </Fragment>
     );
   }
 
+  renderSecondaryBadge = (primaryBadgeType) => {
+    const { classes, product } = this.props;
+    const status = inventoryStatus(product) || {};
+
+    if (primaryBadgeType === "SALE") {
+      if (isProductLowQuantity(product)) {
+        return (
+          <Fragment>
+            {status && <Badge badgeClasses={classes.secondaryBadge} label={"Low Inventory"} />}
+          </Fragment>
+        );
+      }
+      if (isProductBestSeller()) {
+        return (
+          <Fragment>
+            {status && <Badge badgeClasses={classes.secondaryBadge} label={"Best Seller"} />}
+          </Fragment>
+        );
+      }
+    }
+
+    if (primaryBadgeType === "LOW_QUANTITY") {
+      if (isProductBestSeller()) {
+        return (
+          <Fragment>
+            {status && <Badge badgeClasses={classes.secondaryBadge} label={"Best Seller"} />}
+          </Fragment>
+        );
+      }
+    }
+
+    return null;
+  }
+
   render() {
-    const { children, classes } = this.props;
+    const { children, classes, product } = this.props;
+    const status = inventoryStatus(product) || {};
+
+    const badgeOverlayClasses = classNames({
+      [classes.badgeOverlay]: true,
+      [classes.faded]: status.type === INVENTORY_STATUS.SOLD_OUT
+    });
 
     return (
-      <div className={classes.badgeOverlay}>
+      <div className={badgeOverlayClasses}>
         {this.renderBadge()}
         {children}
       </div>
