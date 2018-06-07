1293be5bf792f8927439f2a6c52b33b48461e86a
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-06-07 14:30:01 -0700

    test updates

--- src/components/BadgeOverlay/BadgeOverlay.test.js
+++ src/components/BadgeOverlay/BadgeOverlay.test.js
@@ -1,23 +1,16 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import theme from "lib/theme/reactionTheme";
 import product from "components/ProductDetail/__mocks__/productData.mock";
-import ProductItem from "./ProductItem";
+import BadgeOverlay from "./BadgeOverlay";
 
-const uiStore = {
-  appConfig: {
-    publicRuntimeConfig: {
-      externalAssetsUrl: "http://localhost:3000",
-      placeholderImageUrls: {
-        galleryFeatured: "/resources/placeholder.gif",
-        productGrid: "/resources/placeholder.gif"
-      }
-    }
-  }
-};
 
 test("basic snapshot", () => {
   const component = renderer.create((
-    <ProductItem currencyCode="USD" product={product} uiStore={uiStore}/>
+    <MuiThemeProvider theme={theme}>
+      <BadgeOverlay product={product}/>
+    </MuiThemeProvider>
   ));
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
