4ed9627db00a30f62b242067bd363d9fc4595286
Nat Hamilton <info@nathamilton.com>
2018-06-08 18:00:01 -0500

    chore: removed old Image component files

--- src/components/Image/Image.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import Image from "./Image";
-
-test("Image snapshot", () => {
-  const component = renderer.create(<Image />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
