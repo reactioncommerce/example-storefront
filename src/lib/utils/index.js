3c35a64748bc898f99ed0100d2267df24d4bf209
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-06-08 08:53:40 -0700

    add references to tickets for TODO's

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -36,6 +36,7 @@ export function badgeStatus(product) {
 /**
  * Determines if a product is a best seller.
  * TODO: this is a placeholder, as we don't have "Best Seller" at this moment
+ * https://github.com/reactioncommerce/reaction-next-starterkit/issues/130
  *
  * @param {Object} product - The product
  * @returns {Boolean} - Indicates whether the product is a best seller
@@ -65,6 +66,7 @@ export function isProductLowQuantity(product) {
 /**
  * Determines if a product is on sale.
  * TODO: this is a placeholder, as we don't have "Best Seller" at this moment
+ * https://github.com/reactioncommerce/reaction-next-starterkit/issues/130
  *
  * @param {Object} product - The product
  * @returns {Boolean} - Indicates whether the product is on sale
