fc4767b5e65bce2b91557a1e255acb924f7ca8fd
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-06-10 13:50:23 -0700

    isLowQuantity tests

---
+++ src/lib/utils/isProductLowQuantity.test.js
@@ -0,0 +1,19 @@
+import product from "components/ProductDetail/__mocks__/productData.mock";
+import isProductLowQuantity from "./isProductLowQuantity";
+
+const isLowQuantity = Object.assign({}, product, { isLowQuantity: true, isSoldOut: false });
+const isNotLowQuantity = Object.assign({}, product, { isLowQuantity: false, isSoldOut: false });
+
+test("isProductLowQuantity should return false", () => {
+  const callFunction = isProductLowQuantity(isNotLowQuantity);
+
+  expect(typeof isProductLowQuantity).toBe("function");
+  expect(callFunction).toEqual(false);
+});
+
+test("isProductLowQuantity should return true", () => {
+  const callFunction = isProductLowQuantity(isLowQuantity);
+
+  expect(typeof isProductLowQuantity).toBe("function");
+  expect(callFunction).toEqual(true);
+});
