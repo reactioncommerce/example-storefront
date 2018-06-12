aca13fe600c3f4efa53c0e801bd9a0b836612e64
Jeremy Shimko <jeremy.shimko@gmail.com>
2018-06-12 19:18:50 -0400

    remove more old cookie handling

--- src/server.js
+++ src/server.js
@@ -1,33 +1,20 @@
 import express from "express";
 import nextApp from "next";
 import { useStaticRendering } from "mobx-react";
-import passport from "passport";
-import CookieStrategy from "passport-cookie";
 import logger from "lib/logger";
 import { appPath, dev } from "./config";
 import router from "./routes";
 
 const app = nextApp({ dir: appPath, dev });
 const routeHandler = router.getRequestHandler(app);
-const handle = app.getRequestHandler();
 
 useStaticRendering(true);
-
-passport.use(new CookieStrategy((token, done) => {
-  done(null, { token });
-}));
 
 app
   .prepare()
   .then(() => {
     const server = express();
     server.use(routeHandler);
-
-    server.get(
-      "*",
-      passport.authenticate("cookie", { session: false }),
-      (req, res) => handle(req, res)
-    );
 
     return server.listen(4000, (err) => {
       if (err) throw err;
