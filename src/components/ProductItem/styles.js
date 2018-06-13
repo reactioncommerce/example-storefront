7d1877cd48c85e8a3b363052d675edd2710cc284
Nat Hamilton <info@nathamilton.com>
2018-06-13 16:37:59 -0500

    refactor: updated the ProductItem to have a simpler primary image look up, removed the uiStore and mobx dep

--- src/components/ProductItem/styles.js
+++ src/components/ProductItem/styles.js
@@ -1,8 +1,4 @@
 export const styles = (theme) => ({
-  "@keyframes spin": {
-    from: { transform: "rotate(0deg)" },
-    to: { transform: "rotate(-360deg)" }
-  },
   "root": {},
   "productInfo": {
     display: "flex",
@@ -13,31 +9,6 @@ export const styles = (theme) => ({
   "productMedia": {
     backgroundColor: theme.palette.primary.contrastText,
     position: "relative"
-  },
-  "img": {
-    width: "100%",
-    height: "auto"
-  },
-  "imgLoading": {
-    alignItems: "center",
-    backgroundColor: "transparent",
-    display: "flex",
-    fontSize: 48,
-    height: 100,
-    justifyContent: "center",
-    left: "calc(50% - 50px)",
-    position: "absolute",
-    top: "calc(50% - 50px)",
-    width: 100
-  },
-  "loadingIcon": {
-    color: theme.palette.primary.light,
-    fontSize: "inherit",
-    animationName: "spin",
-    animationDuration: theme.transitions.duration.standard * 2,
-    animationTimingFunction: theme.transitions.easing.sharp,
-    animationIterationCount: "infinite",
-    animationFillMode: "both"
   },
   "link": {
     ...theme.typography.body2,
