5a91819b1d2d24b6c7b1078647bce3f0960cfc7d
Will Lopez <will.lopez77@gmail.com>
2018-06-13 10:19:52 -0700

    Initial groundwork for cart page.

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const routes = require("next-routes")();
 
 routes
+  .add("cart", "/cart", "cart")
   .add("shopProduct", "/shop/:shopSlug/product/:slugOrId", "product")
   .add("product", "/product/:slugOrId/:variantId?", "product")
   .add("shop", "/shop/:shopId/:tag", "index")
