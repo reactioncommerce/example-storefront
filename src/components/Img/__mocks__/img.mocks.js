92442146f2fb83309cde8918135d2280ae7f0469
Nat Hamilton <info@nathamilton.com>
2018-06-14 16:49:18 -0500

    chore: removed img mocks

--- src/components/Img/__mocks__/img.mocks.js
+++
@@ -1,9 +0,0 @@
-export const mockImg = {
-  URLs: {
-    large: "https://picsum.photos/350/350?image=0",
-    medium: "https://picsum.photos/300/300?image=0",
-    original: "https://picsum.photos/1500/1500?image=0",
-    small: "https://picsum.photos/550/550?image=0",
-    thumbnail: "https://picsum.photos/50/50?image=0"
-  }
-};
