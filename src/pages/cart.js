bc87afe27efd412fbb882b7c7eed40add91e3548
Will Lopez <will.lopez77@gmail.com>
2018-07-01 20:53:50 -0700

    Add cart summary and checkout button

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,28 +1,51 @@
 import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
 import trackProductViewed from "lib/tracking/trackProductViewed";
+import CartSummary from "@reactioncommerce/components/CartSummary/v1";
+import Button from "@reactioncommerce/components/Button/v1";
+import CartCheckoutButton from "@reactioncommerce/components/CartCheckoutButton/v1";
 
-const styles = (theme) => ({
+const styles = () => ({
   root: {
-    display: "flex"
   }
 });
 
 @trackProductViewed()
 @withStyles(styles)
 class CartPage extends Component {
+
+  handleCheckOut = () => {
+    // TODO: handle checkout flow.
+  }
+
   render() {
     const { classes } = this.props;
     return (
       <section className={classes.root}>
-        <h1>Shopping Cart</h1>
+        <Typography variant="title" align="center">
+          Shopping Cart
+        </Typography>
         <Grid container spacing={24}>
-          <Grid item xs={8}>
+          <Grid item xs={12} md={8}>
             <h3>Cart Items</h3>
           </Grid>
-          <Grid item xs={4}>
-            <h3>Cart Summary</h3>
+          <Grid item xs={12} md={3}>
+            <Typography component="div" variant="body1">
+              <CartSummary
+                displayShipping="$10.99"
+                displaySubtotal="$275.77"
+                displayTotal="$286.10"
+                itemsQuantity={3}
+              />
+              <CartCheckoutButton
+                components={{ Button }}
+                isDisabled={false}
+                isFullWidth
+                onClick={this.handleCheckOut}
+              />
+            </Typography>
           </Grid>
         </Grid>
       </section>
