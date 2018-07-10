06ae0b1786e8479af7cd9c665192fe0ffa74dc7e
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-07-10 11:42:34 -0700

    update test snapshots

--- src/components/CartPopover/CartPopover.test.js
+++ src/components/CartPopover/CartPopover.test.js
@@ -5,115 +5,55 @@ import { Provider } from "mobx-react";
 import theme from "lib/theme/reactionTheme";
 import CartPopover from "./CartPopover";
 
-const tags = {
-  edges: [
+const cartItem = {
+  _id: "abcdefghijklmnop",
+  attributes: [
     {
-      node: {
-        _id: "v2hE7fzL6cJniwgSm",
-        name: "Tag A",
-        slug: "tag-a",
-        shopId: "J8Bhq3uTtdgwZx3rz",
-        isTopLevel: true,
-        updatedAt: "2018-06-11T18:01:09.568Z",
-        createdAt: "2018-06-11T18:00:48.414Z",
-        isDeleted: false,
-        isVisible: true,
-        groups: [],
-        subTagIds: [
-          "s3nybDz4QPkmapETc",
-          "JB4FRBiWduNsxhhhn",
-          "gRM3ADcY77eNJ7Brm"
-        ]
-      }
-    }, {
-      node: {
-        _id: "s3nybDz4QPkmapETc",
-        name: "Tag A-1",
-        slug: "tag-a-1",
-        shopId: "J8Bhq3uTtdgwZx3rz",
-        isTopLevel: false,
-        updatedAt: "2018-06-11T18:01:02.411Z",
-        createdAt: "2018-06-11T18:01:02.411Z",
-        isDeleted: false,
-        isVisible: true,
-        groups: []
-      }
-    }, {
-      node: {
-        _id: "JB4FRBiWduNsxhhhn",
-        name: "Tag A-2",
-        slug: "tag-a-2",
-        shopId: "J8Bhq3uTtdgwZx3rz",
-        isTopLevel: false,
-        updatedAt: "2018-06-11T18:01:06.975Z",
-        createdAt: "2018-06-11T18:01:06.975Z",
-        isDeleted: false,
-        isVisible: true,
-        groups: []
-      }
-    }, {
-      node: {
-        _id: "gRM3ADcY77eNJ7Brm",
-        name: "Tag A-3",
-        slug: "tag-a-3",
-        shopId: "J8Bhq3uTtdgwZx3rz",
-        isTopLevel: false,
-        updatedAt: "2018-06-11T18:01:09.565Z",
-        createdAt: "2018-06-11T18:01:09.565Z",
-        isDeleted: false,
-        isVisible: true,
-        groups: []
-      }
+      label: "Color",
+      value: "Red"
+    },
+    {
+      label: "Season",
+      value: "Summer"
     }
-  ]
+  ],
+  currentQuantity: 10,
+  imageUrl: "http://localhost:3000/assets/files/Media/nbYKLrZST5DSF87md/thumbnail/chuttersnap-265339-unsplash.png",
+  isLowInventoryQuantity: false,
+  price: {
+    compareAtPrice: "19.99",
+    displayPrice: "18.99"
+  },
+  productSlug: "product-slug",
+  title: "Item Title",
+  quantity: 10
 };
 
-const routingStore = {
-  tag: {
-    _id: "v2hE7fzL6cJniwgSm",
-    name: "Tag A",
-    slug: "tag-a",
-    shopId: "J8Bhq3uTtdgwZx3rz",
-    isTopLevel: true,
-    updatedAt: "2018-06-11T18:01:09.568Z",
-    createdAt: "2018-06-11T18:00:48.414Z",
-    isDeleted: false,
-    isVisible: true,
-    groups: [],
-    relatedTagIds: [
-      "s3nybDz4QPkmapETc",
-      "JB4FRBiWduNsxhhhn",
-      "gRM3ADcY77eNJ7Brm"
-    ]
-  }
+const uiStore = {
+  isCartPopoverOpen: true
 };
 
-test("tagGrid top level tag", () => {
-  const nodes = tags.edges.map((edge) => edge.node);
-  const tag = nodes.find((findTag) => findTag._id === "v2hE7fzL6cJniwgSm");
+const uiStoreFalse = {
+  isCartPopoverOpen: false
+};
 
+test("show cartpopover", () => {
   const component = renderer.create((
     <MuiThemeProvider theme={theme}>
-      <Provider primaryShopId={"J8Bhq3uTtdgwZx3rz"} routingStore={routingStore}>
-        <CartPopover isTagGrid={true} tag={tag} tags={tags} />
-      </Provider>
+      <CartPopover cartItem={cartItem} uiStore={uiStore} />
     </MuiThemeProvider>
   ));
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
 
-test("tagGrid secondlevel tag", () => {
-  const nodes = tags.edges.map((edge) => edge.node);
-  const tag = nodes.find((findTag) => findTag._id === "s3nybDz4QPkmapETc");
-
+test("do not show cartpopover", () => {
   const component = renderer.create((
     <MuiThemeProvider theme={theme}>
-      <Provider routingStore={routingStore}>
-        <CartPopover isTagGrid={true} tag={tag} tags={tags} />
-      </Provider>
+      <CartPopover cartItem={cartItem} uiStore={uiStoreFalse} />
     </MuiThemeProvider>
   ));
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
+
