c350cd32eb5b7b25ab22747b14bb959488d66ea5
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-07-09 22:29:40 -0700

    add isCartPopover info to uiStore

--- src/lib/stores/UIStore.js
+++ src/lib/stores/UIStore.js
@@ -23,6 +23,14 @@ class UIStore {
   @observable isCartOpen = false;
 
   /**
+   * Is the cart popover open or closed
+   *
+   * @type Boolean
+   * @default false
+   */
+  @observable isCartPopoverOpen = false;
+
+  /**
    * Is the menu drawer open or closed
    *
    * @type Boolean
@@ -83,6 +91,14 @@ class UIStore {
 
   @action toggleCartOpen() {
     this.isCartOpen = !this.isCartOpen;
+  }
+
+  @action openCartPopover() {
+    this.isCartPopoverOpen = true;
+  }
+
+  @action closeCartPopover() {
+    this.isCartPopoverOpen = false;
   }
 
   @action closeMenuDrawer() {
