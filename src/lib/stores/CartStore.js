e732518b6323dc746c6c7aaa721d846a73bf10f9
Mike Murray <hellomikemurray@gmail.com>
2018-07-18 15:45:32 -0700

    feat: add storage for cart token, and refactor to simplify logic

--- src/lib/stores/CartStore.js
+++ src/lib/stores/CartStore.js
@@ -2,54 +2,78 @@ import Cookies from "js-cookie";
 import { observable, action } from "mobx";
 
 /**
- * A mobx store for cart
+ * A MobX store for cart
  * @class CartStore
  */
-
 class CartStore {
-  constructor(name) {
-    this.tokenName = name || "token";
-  }
+  CART_ID_KEY_NAME = "cartId";
+  CART_TOKEN_KEY_NAME = "cartToken";
+
   /**
-  * The cartId for an anonymous cart
-  *
-  * @type String
-  */
-  @observable cartId = "";
+   * The cartId for an anonymous cart
+   *
+   * @type String
+   */
+  @observable cartId = null;
 
-  @action setCartId(token) {
-    this.token = token || "";
-    this.saveCartIdToLocalStorage();
-    this.saveCartIdToCookie();
-  }
+  /**
+   * The cartId for an anonymous cart
+   *
+   * @type String
+   */
+  @observable token = null;
 
-  @action unsetCartId() {
-    this.setCartId("");
-  }
+  /**
+   * @name setAnonymousCartCredentials
+   * @summary Set cartID and token to local storage and cookies
+   * @param {String} cartId Cart Id from "createCart" mutation
+   * @param {String} token  Token from "createCart" mutation
+   * @returns {undefined} No return
+   */
+  @action setAnonymousCartCredentials(cartId, token) {
+    this.cartId = cartId || null;
+    this.token = token || null;
 
-  saveCartIdToLocalStorage() {
-    if (typeof this.token === "string" && this.token.length) {
-      localStorage.setItem(this.tokenName, this.token);
+    if (typeof this.cartId === "string" && this.cartId.length) {
+      // Save to local storage
+      localStorage.setItem(this.CART_ID_KEY_NAME, this.cartId);
+      localStorage.setItem(this.CART_TOKEN_KEY_NAME, this.token);
+
+      // Save cookies
+      Cookies.set(this.CART_ID_KEY_NAME, this.cartId);
+      Cookies.set(this.CART_TOKEN_KEY_NAME, this.token);
     } else {
-      localStorage.removeItem(this.tokenName);
+      // Remove from local storage
+      localStorage.removeItem(this.CART_ID_KEY_NAME);
+      localStorage.removeItem(this.CART_TOKEN_KEY_NAME);
+
+      // Remove cookies
+      Cookies.remove(this.CART_ID_KEY_NAME);
+      Cookies.remove(this.CART_TOKEN_KEY_NAME);
     }
   }
 
-  setCartIdFromLocalStorage() {
-    const token = localStorage.getItem(this.tokenName);
-    this.setCartId(token);
+  /**
+   * @name setAnonymousCartCredentialsFromLocalStorage
+   * @summary Set into class properties from local storage
+   * @returns {undefined} No return
+   */
+  setAnonymousCartCredentialsFromLocalStorage() {
+    const cartId = localStorage.getItem(this.CART_ID_KEY_NAME);
+    const token = localStorage.getItem(this.CART_TOKEN_KEY_NAME);
+
+    // Call to set, and ensure all credentials are the same in
+    // local storage, cookies, and class properties
+    this.setAnonymousCartCredentials(cartId, token);
   }
 
-  saveCartIdToCookie() {
-    if (typeof this.token === "string" && this.token.length) {
-      Cookies.set(this.tokenName, this.token);
-    } else {
-      Cookies.remove(this.tokenName);
-    }
-  }
-
+  /**
+   * @name hasAnonymousCart
+   * @summary Return a boolean is the credentials for an anonymous cart exists
+   * @returns {Boolean} Boolean true if credentials for an anonymous cart exists
+   */
   get hasAnonymousCart() {
-    return (this.cartId && this.cartId.length) || false;
+    return (this.cartId && this.token) || false;
   }
 }
 
