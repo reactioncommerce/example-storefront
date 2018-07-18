65ed9978048e9531e2136052f1f6b3d6b3e0fc81
Will Lopez <will.lopez77@gmail.com>
2018-07-18 15:59:41 -0700

    refactor: replace placeholders with actual components.

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -2,10 +2,22 @@ import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import { observer } from "mobx-react";
 import Helmet from "react-helmet";
+import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
+import CheckoutTopHat from "@reactioncommerce/components/CheckoutTopHat/v1";
+import ShopLogo from "@reactioncommerce/components/ShopLogo/v1";
+
+const styles = ({
+  shopLogo: {
+    display: "flex",
+    justifyContent: "center",
+    padding: "2rem 0"
+  }
+});
 
 @observer
+@withStyles(styles)
 class Checkout extends Component {
   static propTypes = {
     classes: PropTypes.object,
@@ -16,7 +28,7 @@ class Checkout extends Component {
   };
 
   render() {
-    const { shop } = this.props;
+    const { classes, shop } = this.props;
 
     return (
       <Fragment>
@@ -25,8 +37,10 @@ class Checkout extends Component {
           <meta name="description" content={shop && shop.description} />
         </Helmet>
         <section>
-          <div>Placeholder for CheckoutTopHat component</div>
-          <div>Placeholder for CheckoutNav component</div>
+          <CheckoutTopHat checkoutMessage="Free Shipping + Free Returns" />
+          <div className={classes.shopLogo}>
+            <ShopLogo shopName={shop.name} />
+          </div>
           <Grid container spacing={24}>
             <Grid item xs={12} md={8}>
               <Typography>
