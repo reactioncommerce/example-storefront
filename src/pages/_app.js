2d55a058f148f3b1dcd85e920c3b911c75aad2ab
Mike Murray <hellomikemurray@gmail.com>
2018-07-24 20:54:27 -0700

    feat: add `withViewer` decorator
    - Add `withViewer` decorator
    - Move `withMobX`

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,15 +5,17 @@ import dispatch from "lib/tracking/dispatch";
 import withApolloClient from "lib/apollo/withApolloClient";
 import withTheme from "lib/theme/withTheme";
 import withShop from "containers/shop/withShop";
+import withViewer from "containers/account/withViewer";
 import withTags from "containers/tags/withTags";
 import Layout from "components/Layout";
 import withMobX from "lib/stores/withMobX";
 import rootMobXStores from "lib/stores";
 
 @withApolloClient
-@withShop
-@withTags
 @withMobX
+@withShop
+@withViewer
+@withTags
 @withTheme
 @track({}, { dispatch })
 export default class App extends NextApp {
