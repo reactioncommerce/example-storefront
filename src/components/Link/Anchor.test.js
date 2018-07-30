5defdec0aeb922afd4f7c11ad64d8235b7aec83a
Will Lopez <will.lopez77@gmail.com>
2018-07-30 16:52:05 -0700

    fix: update test snapshots

--- src/components/Link/Anchor.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import Anchor from "./Anchor";
-
-test("basic snapshot", () => {
-  const component = renderer.create(<Anchor href="/next">Next</Anchor>);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
