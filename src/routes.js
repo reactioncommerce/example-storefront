0dbbceba2528d35e0922f341304a50e2a5d2e8d6
Will Lopez <will.lopez77@gmail.com>
2018-08-03 14:55:53 -0700

    refactor: add home route and add page name to checkout route.

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 const routes = require("next-routes")();
 
 routes
+  .add("home", "/", "index")
   .add("cart", "/cart", "cart")
-  .add("checkout", "/cart/checkout")
+  .add("checkout", "/cart/checkout", "checkout")
   .add("shopProduct", "/shop/:shopSlug/product/:slugOrId", "product")
   .add("product", "/product/:slugOrId/:variantId?", "product")
   .add("shop", "/shop/:shopId/:tag", "index")
