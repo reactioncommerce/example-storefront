ea6503a7dcf71e85075634b12edfe204e72602ea
Jeff Corpuz <jeffcorpuz@icloud.com>
2018-08-09 18:10:25 -0400

    Fix linting errors

--- tests/ssr/next.test.js
+++ tests/ssr/next.test.js
@@ -1,23 +1,20 @@
 /*
  *   Test to Verify NextJS rendering pages via SSR
  */
+const cheerio = require("cheerio");
+const chai = require("chai");
+const request = require("request");
 
-const cheerio = require('cheerio');
-const chai = require('chai');
-const request = require('request');
-let expect = chai.expect
+const expect = chai.expect;
+const url = "localhost:3000";
 
-let url = 'localhost:3000';
-
-describe('NextJS Loading', () => {
-
-    it('SSR Loads with an HTML Body', () => {
-        request(url, (err, resp, body) => {
-            if (!err && resp.statusCode == 200) {
-                let $ = cheerio.load(body);
-                expect($('#__next').find('div')).to.not.be.empty;
-            }
-        });
-    })
-
-})
+describe("NextJS Loading", () => {
+  it("SSR Loads with an HTML Body", () => {
+    request(url, (err, resp, body) => {
+      if (!err && resp.statusCode === 200) {
+        const cheer = cheerio.load(body);
+        expect(cheer("#__next").find("div")).to.not.be.empty;
+      }
+    });
+  });
+});
