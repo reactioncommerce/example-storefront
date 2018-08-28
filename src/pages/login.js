9757717d9e77ed76066a05a4c39d193a72e4ee20
Nat Hamilton <info@nathamilton.com>
2018-08-28 10:35:25 -0500

    feat: styled main content layout, added login/register buttons, added section titles

--- src/pages/login.js
+++ src/pages/login.js
@@ -14,26 +14,51 @@ import Link from "components/Link";
 
 import ChevronLeftIcon from "mdi-material-ui/ChevronLeft";
 
+const flexWrapper = () => ({
+  alignItems: "stretch",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-start"
+});
+
 const styles = (theme) => ({
-  // loginWrapper: {
-  //   maxWidth: "600px",
-  //   alignSelf: "flex-end",
-  //   [theme.breakpoints.up("md")]: {
-  //     paddingRight: "2rem"
-  //   }
-  // },
-  // guestWrapper: {
-  //   maxWidth: "400px",
-  //   alignSelf: "flex-start",
-  //   [theme.breakpoints.up("md")]: {
-  //     paddingRight: "2rem"
-  //   }
-  // },
-  main: {
-    flex: "1 1 auto",
-    maxWidth: theme.layout.mainContentMaxWidth,
-    margin: "0 auto",
-    padding: `0 ${theme.spacing.unit * 3}px`
+  loginWrapper: {
+    ...flexWrapper(theme),
+    paddingBottom: theme.spacing.unit * 8,
+    [theme.breakpoints.up("md")]: {
+      minHeight: "400px",
+      paddingBottom: 0,
+      paddingRight: theme.spacing.unit * 8
+    }
+  },
+  guestWrapper: {
+    ...flexWrapper(theme),
+    borderTop: `solid 1px ${theme.palette.reaction.black10}`,
+    paddingTop: theme.spacing.unit * 8,
+    [theme.breakpoints.up("md")]: {
+      borderLeft: `solid 1px ${theme.palette.reaction.black10}`,
+      borderTop: "none",
+      paddingLeft: theme.spacing.unit * 8,
+      paddingTop: 0
+    }
+  },
+  backLink: {
+    color: theme.palette.reaction.black80,
+    cursor: "pointer",
+    fontFamily: theme.typography.fontFamily,
+    fontSize: 14,
+    "&:hover": {
+      color: theme.palette.reaction.reactionBlue400
+    }
+  },
+  backLinkText: {
+    letterSpacing: "0.3px",
+    lineHeight: 1.71,
+    marginLeft: theme.spacing.unit,
+    textDecoration: "underline"
+  },
+  loginButton: {
+    marginTop: theme.spacing.unit * 3
   },
   headerCenter: {},
   headerFlex: {
@@ -48,24 +73,22 @@ const styles = (theme) => ({
     marginBottom: theme.spacing.unit * 3,
     padding: theme.spacing.unit * 3
   },
-  backLink: {
-    color: theme.palette.reaction.black80,
-    cursor: "pointer",
-    fontFamily: theme.typography.fontFamily,
-    fontSize: 14
-  },
-  backLinkText: {
-    letterSpacing: "0.3px",
-    lineHeight: 1.71,
-    marginLeft: theme.spacing.unit,
-    textDecoration: "underline"
-  },
-
   logo: {
     color: theme.palette.reaction.reactionBlue,
     margin: "auto",
     borderBottom: `solid 5px ${theme.palette.reaction.reactionBlue200}`
-  }
+  },
+  main: {
+    flex: "1 1 auto",
+    maxWidth: theme.layout.mainLoginMaxWidth,
+    minHeight: "calc(100vh - 135px)",
+    margin: "0 auto",
+    padding: `${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px 0`,
+    [theme.breakpoints.up("md")]: {
+      padding: `${theme.spacing.unit * 10}px ${theme.spacing.unit * 3}px 0`
+    }
+  },
+  root: {}
 });
 
 @withCart
@@ -93,13 +116,25 @@ export default class Login extends Component {
     Router.back();
   };
 
+  handleLoginClick = () => {
+    // TODO: Redirect to Auth solution
+    // eslint-disable-next-line
+    console.log("login buttton clicked!");
+  };
+
+  handleRegisterClick = () => {
+    // TODO: Redirect to Auth solution
+    // eslint-disable-next-line
+    console.log("register buttton clicked!");
+  };
+
   renderHeader() {
-    const { classes, shop, theme } = this.props;
+    const { classes, shop } = this.props;
     return (
       <div className={classes.header}>
         <div className={classes.headerFlex}>
           <Link className={classes.backLink} onClick={this.handleBackClick}>
-            <ChevronLeftIcon style={{ fontSize: 18, color: theme.palette.reaction.black80, verticalAlign: "sub" }} />
+            <ChevronLeftIcon style={{ fontSize: 18, color: "inherit", verticalAlign: "sub", transition: "none" }} />
             <span className={classes.backLinkText}>Back</span>
           </Link>
         </div>
@@ -118,19 +153,31 @@ export default class Login extends Component {
   renderCheckoutLogin() {
     const { classes, setEmailOnAnonymousCart } = this.props;
     return (
-      <Grid container spacing={24}>
+      <Grid container>
         <Grid item xs={12} md={7}>
-          <div className={classes.flexContainer}>
-            <div className={classes.loginWrapper}>
-              <Button onClick={() => Router.back()}>Back!</Button>
-            </div>
+          <div className={classes.loginWrapper}>
+            <Typography variant="title" gutterBottom>
+              Returning Customer
+            </Typography>
+            <Button onClick={this.handleLoginClick} actionType="important" isFullWidth className={classes.loginButton}>
+              Login
+            </Button>
+            <Button
+              onClick={this.handleRegisterClick}
+              actionType="secondary"
+              isFullWidth
+              className={classes.loginButton}
+            >
+              Create a new account
+            </Button>
           </div>
         </Grid>
         <Grid item xs={12} md={5}>
-          <div className={classes.flexContainer}>
-            <div className={classes.guestWrapper}>
-              <GuestForm onSubmit={setEmailOnAnonymousCart} />
-            </div>
+          <div className={classes.guestWrapper}>
+            <Typography variant="title" gutterBottom>
+              Guest Checkout
+            </Typography>
+            <GuestForm onSubmit={setEmailOnAnonymousCart} />
           </div>
         </Grid>
       </Grid>
