44bf0206874b732237a137f49c32416e6582743c
Mike Murray <hellomikemurray@gmail.com>
2018-09-13 22:40:40 -0700

    refactor: remove cart prop type definition

--- src/components/OrderFulfillmentGroups/OrderFulfillmentGroups.js
+++ src/components/OrderFulfillmentGroups/OrderFulfillmentGroups.js
@@ -4,17 +4,6 @@ import OrderFulfillmentGroup from "components/OrderFulfillmentGroup";
 
 class OrderFulfillmentGroups extends Component {
   static propTypes = {
-    cart: PropTypes.shape({
-      items: PropTypes.arrayOf(PropTypes.object),
-      checkout: PropTypes.shape({
-        itemTotal: PropTypes.shape({
-          displayAmount: PropTypes.string
-        }),
-        taxTotal: PropTypes.shape({
-          displayAmount: PropTypes.string
-        })
-      })
-    }),
     classes: PropTypes.object,
     order: PropTypes.shape({
       fulfillmentGroups: PropTypes.arrayOf(PropTypes.object)
