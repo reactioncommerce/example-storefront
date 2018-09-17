7027d09c8ce349c3cc3ddac3a103b1c91ea6b3df
Nat Hamilton <info@nathamilton.com>
2018-09-17 14:42:27 -0500

    feat: Moved login/reg btn & guestForm into new Entry component

---
+++ src/components/Entry/Entry.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import theme from "lib/theme/reactionTheme";
+import Entry from "./";
+
+test("basic snapshot", () => {
+  const component = renderer.create(
+    shallow(
+      <MuiThemeProvider theme={theme}>
+        <Entry />
+      </MuiThemeProvider>
+    ).get(3)
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
