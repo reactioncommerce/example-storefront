77f806101b4a0b2f053c918c1b28bc7ae2f032aa
Nat Hamilton <info@nathamilton.com>
2018-09-18 18:27:46 -0500

    test: fixed snapshot to output something

--- src/components/Entry/Entry.test.js
+++ src/components/Entry/Entry.test.js
@@ -1,14 +1,17 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { shallow } from "enzyme";
+import { ComponentsProvider } from "@reactioncommerce/components-context";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import theme from "lib/theme/reactionTheme";
+import components from "../../lib/theme/components";
 import Entry from "./";
 
 test("basic snapshot", () => {
-  const component = renderer.create(shallow(<MuiThemeProvider theme={theme}>
-    <Entry />
-  </MuiThemeProvider>).get(3));
+  const component = renderer.create(<ComponentsProvider value={components}>
+    <MuiThemeProvider theme={theme}>
+      <Entry setEmailOnAnonymousCart={() => {}} />
+    </MuiThemeProvider>
+  </ComponentsProvider>);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
