76357b08ee70708101c02f31d9dfdda686f5c100
Mike Murray <hellomikemurray@gmail.com>
2018-09-19 11:03:37 -0700

    feat: add style name

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -19,7 +19,7 @@ const styles = (theme) => ({
   }
 });
 
-@withStyles(styles)
+@withStyles(styles, { name: "SkLayout" })
 class Layout extends Component {
   static propTypes = {
     children: PropTypes.node,
