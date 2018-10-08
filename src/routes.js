d78378b829a62e10a2c12e0bfb504dd1405c659f
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-10-08 14:59:35 -0700

    add new profileAddressBook page to routes

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ routes
   .add("login", "/login", "login")
   .add("shopProduct", "/shop/:shopSlug/product/:slugOrId", "product")
   .add("product", "/product/:slugOrId/:variantId?", "product")
+  .add("addressBook", "/profile/address", "profileAddressBook")
   .add("shop", "/shop/:shopId/:tag", "productGrid")
   .add("tag", "/tag/:slug", "tag");
 
