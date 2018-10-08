ee29a1fed6c21ddf7f756f10ff9f156e35f592d6
Mike Murray <hellomikemurray@gmail.com>
2018-10-08 08:57:37 -0700

    test: fix broken test

--- src/components/NavigationMobile/NavigationMobile.test.js
+++ src/components/NavigationMobile/NavigationMobile.test.js
@@ -1,10 +1,16 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 import { shallow } from "enzyme";
+import theme from "lib/theme/reactionTheme";
 import NavigationMobile from "./NavigationMobile";
 
 test("basic snapshot", () => {
-  const component = renderer.create(shallow(<NavigationMobile />).get(3));
+  const component = renderer.create(shallow((
+    <MuiThemeProvider theme={theme}>
+      <NavigationMobile />
+    </MuiThemeProvider>
+  )).get(3));
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
