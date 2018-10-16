706c8f41a5c23a03f6714a7d13db760fb4b2d5a9
Erik Kieckhafer <erik.kieckhafer@gmail.com>
2018-10-15 21:33:09 -0700

    add routes for addressBook and orders on profile

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,8 @@ routes
   .add("login", "/login", "login")
   .add("shopProduct", "/shop/:shopSlug/product/:slugOrId", "product")
   .add("product", "/product/:slugOrId/:variantId?", "product")
-  .add("addressBook", "/profile/address", "profileAddressBook")
+  .add("profileAddressBook", "/profile/address", "profile")
+  .add("profileOrders", "/profile/orders", "profile")
   .add("shop", "/shop/:shopId/:tag", "productGrid")
   .add("tag", "/tag/:slug", "tag");
 
