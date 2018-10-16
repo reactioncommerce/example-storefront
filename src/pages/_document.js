dfe7d3123fe3e9f52ebedb096cf67515a7282097
Will Lopez <will.lopez77@gmail.com>
2018-10-16 14:44:16 -0700

    fix: load non-critical scripts asyncronously

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -108,9 +108,9 @@ class HTMLDocument extends Document {
           {scripts.map((script, index) =>
             (script.innerHTML ? (
             /* eslint-disable-next-line */
-              <script key={index} type={script.type} dangerouslySetInnerHTML={{ __html: script.innerHTML }} />
+              <script async key={index} type={script.type} dangerouslySetInnerHTML={{ __html: script.innerHTML }} />
             ) : (
-              <script key={index} {...script} />
+              <script async key={index} {...script} />
             )))}
         </body>
       </html>
