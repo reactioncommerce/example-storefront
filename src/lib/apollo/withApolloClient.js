9a626b1f0d36289540795bb3477cff76993962a3
Seun Martins <seundotme@gmail.com>
2018-11-21 00:24:25 +0100

    Fix incorrect logger.debug call

--- src/lib/apollo/withApolloClient.js
+++ src/lib/apollo/withApolloClient.js
@@ -71,7 +71,8 @@ export default function withApolloClient(WrappedComponent) {
             // In server, if a 401 Unauthorized error occurred, redirect to /signin.
             // This will re-authenticate without showing a login page and a new token is issued.
             if (error.networkError.response.status === 401 && res) {
-              logger.debug("Received 401 error from the GraphQL API due to invalid or expired authentication credentials. Triggering token refresh via redirect flow");
+              // eslint-disable-next-line no-console
+              console.log("Received 401 error from the GraphQL API due to invalid or expired authentication credentials. Triggering token refresh via redirect flow");
               res.writeHead(302, { Location: "/signin" });
               res.end();
               return {};
