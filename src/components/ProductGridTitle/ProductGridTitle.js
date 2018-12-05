97b41a9a5883337441e318a7e20b6c46302eae6a
Machiko Yasuda <machikoyasuda@gmail.com>
2018-12-05 15:24:42 -0800

    feat(productgridtitle): add product grid display title, with mocked title data

---
+++ src/components/ProductGridTitle/ProductGridTitle.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+
+const styles = (theme) => ({
+  gridTitle: {
+    color: theme.palette.reaction.coolGrey500,
+    fontWeight: theme.typography.fontWeightMedium,
+    margin: "30px 0",
+    textAlign: "center"
+  }
+});
+
+@withStyles(styles, { name: "SkProductGridTitle" })
+export default class ProductGridTitle extends Component {
+  static propTypes = {
+    classes: PropTypes.object,
+    tag: PropTypes.object
+  };
+
+  static defaultProps = {
+    classes: {},
+    tag: {
+      displayTitle: "Test Title"
+    }
+  };
+
+  render() {
+    const { classes, tag: { displayTitle } } = this.props;
+
+    if (!displayTitle) return null;
+
+    return (
+      <section className={classes.titleContainer}>
+        <Grid item xs={12}>
+          <Typography className={classes.gridTitle} component="h1" variant="title">{displayTitle}</Typography>
+        </Grid>
+      </section>
+    );
+  }
+}
