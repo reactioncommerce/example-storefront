8256e4f76af466ac0835f0744ad7caf146c949f9
Erik Kieckhafer <ek@ato.la>
2019-02-21 20:37:31 -0800

    update OrderCardSummary to remove styles no longer needed with Component Library updates
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/components/OrderCardSummary/OrderCardSummary.js
+++ src/components/OrderCardSummary/OrderCardSummary.js
@@ -1,27 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
 import CartSummary from "@reactioncommerce/components/CartSummary/v1";
-import styled from "styled-components";
 
-// Use styled components to adjust the styling of the
-// cart summary component to fit inside a bordered box
-// TODO: EK - Get padding on order total
-export const OrderCardSummaryContainer = styled.div`
-  table td {
-    font-size: 14px;
-  }
-  table td span {
-    font-size: 18px;
-  }
-`;
-
-
-const styles = (theme) => ({
-  orderCardSummary: {}
-});
-
-@withStyles(styles)
 class OrderCardSummary extends Component {
   static propTypes = {
     classes: PropTypes.object,
@@ -45,7 +25,7 @@ class OrderCardSummary extends Component {
   }
 
   render() {
-    const { classes, summary } = this.props;
+    const { summary } = this.props;
 
     if (summary) {
       const {
@@ -57,16 +37,14 @@ class OrderCardSummary extends Component {
       } = summary;
 
       return (
-        <OrderCardSummaryContainer className={classes.orderCardSummary}>
-          <CartSummary
-            isDense
-            displayShipping={fulfillmentTotal && fulfillmentTotal.displayAmount}
-            displaySubtotal={itemTotal && itemTotal.displayAmount}
-            displaySurcharge={surchargeTotal && surchargeTotal.displayAmount}
-            displayTax={taxTotal && taxTotal.displayAmount}
-            displayTotal={total && total.displayAmount}
-          />
-        </OrderCardSummaryContainer>
+        <CartSummary
+          isDense
+          displayShipping={fulfillmentTotal && fulfillmentTotal.displayAmount}
+          displaySubtotal={itemTotal && itemTotal.displayAmount}
+          displaySurcharge={surchargeTotal && surchargeTotal.displayAmount}
+          displayTax={taxTotal && taxTotal.displayAmount}
+          displayTotal={total && total.displayAmount}
+        />
       );
     }
 
