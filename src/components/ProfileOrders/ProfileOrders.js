d82ab8080a72db406a0b7888f89fe02cd859185d
Erik Kieckhafer <ek@ato.la>
2019-02-21 16:05:21 -0800

    ProfileOrders component
    Signed-off-by: Erik Kieckhafer <ek@ato.la>

--- src/components/ProfileOrders/ProfileOrders.js
+++ src/components/ProfileOrders/ProfileOrders.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { observer } from "mobx-react";
+import { inject, observer } from "mobx-react";
 import { Typography } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import relayConnectionToArray from "lib/utils/relayConnectionToArray";
 import OrderCard from "components/OrderCard";
+import Select from "components/Select";
 import withOrders from "containers/order/withOrders";
 import ErrorPage from "../../pages/_error";
 
+const styles = (theme) => ({
+  profileOrdersContainer: {},
+  profileOrdersList: {},
+  profileOrdersSelect: {
+    marginBottom: theme.spacing.unit * 4
+  },
+  profileOrdersTitle: {
+    marginBottom: theme.spacing.unit * 4
+  }
+});
+
+@withStyles(styles, { withTheme: true })
+@inject("uiStore")
 @withOrders
 @observer
 class ProfileOrders extends Component {
   static propTypes = {
+    classes: PropTypes.object,
     orders: PropTypes.arrayOf(PropTypes.shape({
       email: PropTypes.string.isRequired,
       fulfillmentGroups: PropTypes.arrayOf(PropTypes.object).isRequired,
@@ -21,8 +37,55 @@ class ProfileOrders extends Component {
     shop: PropTypes.shape({
       name: PropTypes.string.isRequired,
       description: PropTypes.string
-    })
+    }),
+    uiStore: PropTypes.shape({
+      accountProfileOptions: PropTypes.shape({
+        orderStatusQuery: PropTypes.string
+      }),
+      setAccountProfileOrderStatusQueryVariable: PropTypes.func
+    }).isRequired
   };
+
+  handleOrderStatusTypeChange = (event) => {
+    this.props.uiStore.setAccountProfileOrderStatusQueryVariable(event.target.value);
+
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  renderOrderTypeSelect() {
+    const { uiStore: { accountProfileOptions: { orderStatusQuery } } } = this.props;
+
+    const orderStatusOptions = [
+      {
+        name: "All",
+        value: "all"
+      },
+      {
+        name: "Open orders",
+        value: "open"
+      },
+      {
+        name: "Completed orders",
+        value: "completed"
+      },
+      {
+        name: "Canceled orders",
+        value: "canceled"
+      }
+    ];
+
+    return (
+      <Select
+        value={orderStatusQuery}
+        options={orderStatusOptions}
+        inputProps={{
+          name: "orderStatus",
+          id: "order-status"
+        }}
+        onChange={this.handleOrderStatusTypeChange}
+      />
+    );
+  }
 
   renderOrders() {
     const { orders } = this.props;
@@ -34,14 +97,21 @@ class ProfileOrders extends Component {
   }
 
   render() {
-    const { orders, shop } = this.props;
+    const { classes, orders, shop } = this.props;
 
     if (!orders) return <ErrorPage shop={shop} subtitle="No orders found" />;
 
     return (
-      <Grid item xs={12} md={12}>
-        <Typography variant="title">Orders</Typography>
-        {this.renderOrders()}
+      <Grid className={classes.profileOrdersContainer} container>
+        <Grid className={classes.profileOrdersTitle} item xs={12} md={12}>
+          <Typography variant="title">Orders</Typography>
+        </Grid>
+        <Grid className={classes.profileOrdersSelect} item xs={12} md={12}>
+          {this.renderOrderTypeSelect()}
+        </Grid>
+        <Grid className={classes.profileOrdersList} item xs={12} md={12}>
+          {this.renderOrders()}
+        </Grid>
       </Grid>
     );
   }
--- src/components/ProfileOrders/ProfileOrders.test.js
+++ src/components/ProfileOrders/ProfileOrders.test.js
@@ -1,22 +1,72 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { MockedProvider } from "react-apollo/test-utils";
 import { ComponentsProvider } from "@reactioncommerce/components-context";
 import { MuiThemeProvider } from "@material-ui/core/styles";
+import { Provider } from "mobx-react";
 import theme from "custom/reactionTheme";
-import mockComponents from "../../../tests/mockComponents";
+import ordersByAccountId from "containers/order/queries.gql";
+import components from "custom/componentsContext";
 import ProfileOrders from "./ProfileOrders";
 
-const orders = [];
+
+
+const orders = [
+  {
+    account: {
+      _id: "cmVhY3Rpb24vYWNjb3VudDpQQ1hmYmNuUDlIaFphSjRZRw==",
+      fulfillmentGroups: {}
+    }
+  }
+];
+
+const uiStore = {
+  accountProfileOptions: {
+    orderStatusQuery: "all"
+  },
+  language: "en"
+};
+const cartStore = {
+};
+const routingStore = {
+};
+
+const authStore = {
+  accountId: null,
+  isAuthenticated: false
+};
+
+const mocks = [
+  {
+    request: {
+      query: ordersByAccountId,
+      variables: {
+        accountId: "cmVhY3Rpb24vYWNjb3VudDpQQ1hmYmNuUDlIaFphSjRZRw==",
+        language: "en",
+        orderStatus: "all"
+      }
+    },
+    result: {
+      data: {
+        orderData: orders
+      }
+    }
+  }
+];
 
 test("basic snapshot", () => {
   const component = renderer.create((
-    <ComponentsProvider value={mockComponents}>
-      <MuiThemeProvider theme={theme}>
-        <ProfileOrders
-          orders={orders}
-        />
-      </MuiThemeProvider>
-    </ComponentsProvider>
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <ComponentsProvider value={components}>
+        <Provider authStore={authStore} cartStore={cartStore} primaryShopId={"123"} routingStore={routingStore} uiStore={uiStore}>
+          <MuiThemeProvider theme={theme}>
+            <ProfileOrders
+              orders={orders}
+            />
+          </MuiThemeProvider>
+        </Provider>
+      </ComponentsProvider>
+    </MockedProvider>
   ));
 
   const tree = component.toJSON();
