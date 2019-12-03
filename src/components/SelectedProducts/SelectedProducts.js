c6b7ae173da17e16b7b99becfb77e0bcfa901738
mathiasfc <mathias.falci@gmail.com>
2019-12-03 17:07:33 -0300

    :lipstick: :sparkles: fully responsive sub products list

--- src/components/SelectedProducts/SelectedProducts.js
+++ src/components/SelectedProducts/SelectedProducts.js
@@ -1,18 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
-// import Button from "../Button";
+import { Hidden } from "@material-ui/core";
 import * as s from "./style";
-
-const settings = {
-  infinite: false,
-  slidesToShow: 2.5,
-  arrows: false,
-  variableWidth: true,
-  swipeToSlide: true,
-  afterChange: (index) => {
-    console.log(`Slider Changed to: ${index + 1}, background: #222; color: #bada55`);
-  }
-};
 
 const products = [
   {
@@ -52,33 +41,83 @@ const products = [
   }
 ];
 
+const mobileSettings = {
+  infinite: false,
+  slidesToShow: 2.5,
+  arrows: false,
+  variableWidth: true,
+  swipeToSlide: true,
+  afterChange: (index) => {
+    console.log(`Slider Changed to: ${index + 1}, background: #222; color: #bada55`);
+  }
+};
+
+const desktopSettings = {
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  variableWidth: true
+};
+
 const ProductList = () => {
   return (
-    <s.Section>
-      <s.Header>
-        <s.Title>Lorem Ipsum</s.Title>
-        <s.SectionDescription>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
-          aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-        </s.SectionDescription>
-      </s.Header>
+    <div>
+      <Hidden mdUp>
+        <s.MobileSection>
+          <s.Header>
+            <s.Title>Lorem Ipsum</s.Title>
+            <s.SectionDescription>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam
+              rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+              explicabo.
+            </s.SectionDescription>
+          </s.Header>
+          <div>
+            <Slider {...mobileSettings}>
+              {products &&
+                products.length &&
+                products.map((prod, idx) => {
+                  return (
+                    <div>
+                      <s.Product className="product-item" key={idx}>
+                        <s.ImageContainer src={prod.image} />
+                      </s.Product>
+                    </div>
+                  );
+                })}
+            </Slider>
+          </div>
+        </s.MobileSection>
+      </Hidden>
 
-      <div>
-        <Slider {...settings}>
-          {products &&
-            products.length &&
-            products.map((prod, idx) => {
-              return (
-                <div>
-                  <s.Product className="product-item" key={idx}>
-                    <s.ImageContainer src={prod.image} />
-                  </s.Product>
-                </div>
-              );
-            })}
-        </Slider>
-      </div>
-    </s.Section>
+      <Hidden smDown>
+        <s.DesktopSection>
+          <s.Header>
+            <s.Title>Lorem Ipsum</s.Title>
+            <s.SectionDescription>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam
+              rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+              explicabo.
+            </s.SectionDescription>
+          </s.Header>
+          <s.SliderContainer>
+            <Slider {...desktopSettings}>
+              {products &&
+                products.length &&
+                products.map((prod, idx) => {
+                  return (
+                    <s.Product className="product-item" desktopMode key={idx}>
+                      <s.ImageContainer desktopMode src={prod.image} />
+                    </s.Product>
+                  );
+                })}
+            </Slider>
+          </s.SliderContainer>
+        </s.DesktopSection>
+      </Hidden>
+    </div>
   );
 };
 
--- src/components/SelectedProducts/style.js
+++ src/components/SelectedProducts/style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import { breakpoints } from "../../helpers/constants";
 
-export const Section = styled.section`
+export const MobileSection = styled.section`
   background-color: ${({ theme }) => theme.layout.backgrounds.primary};
   padding: 30px 0px;
   .slick-track {
@@ -17,6 +18,46 @@ export const Section = styled.section`
   }
 `;
 
+export const DesktopSection = styled.section`
+  background-color: ${({ theme }) => theme.layout.backgrounds.primary};
+  padding: 30px 0px;
+
+  .slick-track {
+    display: flex;
+  }
+  .slick-slide {
+    padding: 0 8px;
+
+    .product-item {
+      margin-right: 10px;
+    }
+  }
+
+  .slick-slider {
+    .slick-prev::before,
+    .slick-next::before {
+      font-size: 30px;
+      color: ${({ theme }) => theme.typography.colors.price};
+      opacity: 1;
+    }
+
+    .slick-prev {
+      left: -25px !important;
+      z-index: 999;
+    }
+
+    .slick-next {
+      right: -10px;
+      z-index: 999;
+    }
+  }
+`;
+
+export const SliderContainer = styled.div`
+  margin: 0 auto;
+  width: 80%;
+`;
+
 export const Header = styled.div`
   display: flex;
   flex-direction: column;
@@ -29,6 +70,10 @@ export const Title = styled.span`
   font-size: ${({ theme }) => theme.typography.sizes.text.SIZE_1};
   color: ${({ theme }) => theme.typography.colors.secondary};
   font-weight: ${({ theme }) => theme.typography.weights.BOLD};
+
+  @media (min-width: ${breakpoints.md}) {
+    font-size: ${({ theme }) => theme.typography.sizes.title.SIZE_1};
+  }
 `;
 
 export const SectionDescription = styled.span`
@@ -36,17 +81,23 @@ export const SectionDescription = styled.span`
   color: ${({ theme }) => theme.typography.colors.secondary};
   margin-top: 20px;
   line-height: 1.67;
+
+  @media (min-width: ${breakpoints.md}) {
+    font-size: ${({ theme }) => theme.typography.sizes.title.SIZE_4};
+    width: 75%;
+  }
 `;
 
 export const Product = styled.div`
   border-radius: 3px;
   height: auto;
-  width: 157px;
+  width: auto;
+  /* width: ${({ desktopMode }) => (desktopMode ? "auto" : "157px")}; */
 `;
 
 export const ImageContainer = styled.div`
-  width: 156px;
-  height: 166px;
+  width: ${({ desktopMode }) => (desktopMode ? "263px" : "156px")};
+  height: ${({ desktopMode }) => (desktopMode ? "276px" : "166px")};
   position: relative;
   background: ${({ src }) => (src ? `url(${src})` : "")};
   background-size: cover;
