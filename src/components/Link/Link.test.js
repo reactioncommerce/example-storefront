43262f553d81a8be158617b4ec2cec49fb084c65
Janus Reith <janus.reith@googlemail.com>
2020-03-16 20:31:48 +0100

    Remove useless/soon to be outdated tests

--- src/components/Link/Link.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import Link from "./";
-
-test("basic snapshot", () => {
-  const component = renderer.create(<Link route="/next">Linked</Link>);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
