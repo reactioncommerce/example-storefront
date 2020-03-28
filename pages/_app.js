bcbc7d5ea944114bad127a13b8a0ff9afc05575e
Janus Reith <janus.reith@googlemail.com>
2020-03-29 00:50:12 +0100

    Add global Context providers

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,16 @@ import React from "react";
 import { ThemeProvider as RuiThemeProvider } from "styled-components";
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { StripeProvider } from "react-stripe-elements";
 import { Provider as MobxProvider } from "mobx-react";
 import { ComponentsProvider } from "@reactioncommerce/components-context";
 import Layout from "components/Layout";
 import withMobX from "lib/stores/withMobX";
-import withShop from "containers/shop/withShop";
-import rootMobXStores from "lib/stores";
+import { RoutingProvider } from "context/RoutingContext";
+import { AuthProvider } from "context/AuthContext";
+import { CartProvider } from "context/CartContext";
+import { ShopProvider } from "context/ShopContext";
+import { UIProvider } from "context/UIContext";
+import { LocaleProvider } from "context/LocaleContext";
 import components from "custom/componentsContext";
 import componentTheme from "custom/componentTheme";
 import theme from "custom/reactionTheme";
@@ -17,53 +20,51 @@ import theme from "custom/reactionTheme";
 class App extends NextApp {
   constructor(props) {
     super(props);
-    this.state = { stripe: null };
   }
 
   componentDidMount() {
-    // Fetch and update auth token in auth store
-    rootMobXStores.cartStore.setAnonymousCartCredentialsFromLocalStorage();
-
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles && jssStyles.parentNode) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
-
-    if (process.env.STRIPE_PUBLIC_API_KEY && window.Stripe) {
-      // eslint-disable-next-line react/no-did-mount-set-state
-      this.setState({ stripe: window.Stripe(process.env.STRIPE_PUBLIC_API_KEY) });
-    }
   }
 
   render() {
-    const { Component, pageProps, viewer, ...rest } = this.props;
-    const { tags, shop } = pageProps;
-    const { route } = this.props.router;
-    const { stripe } = this.state;
+    const { Component, pageProps, ...rest } = this.props;
+    const { tags, shop, lang, translations, namespaces } = pageProps;
 
     return (
-      <ComponentsProvider value={components}>
-        <MobxProvider suppressChangedStoreWarning navItems={shop && shop.defaultNavigationTree} tags={tags}>
-          <RuiThemeProvider theme={componentTheme}>
-            <MuiThemeProvider theme={theme}>
-              <CssBaseline />
-              {route === "/checkout" || route === "/login" ? (
-                <StripeProvider stripe={stripe}>
-                  <Component shop={shop} {...rest} {...pageProps} />
-                </StripeProvider>
-              ) : (
-                <Layout shop={shop} viewer={viewer}>
-                  <Component shop={shop} {...rest} {...pageProps} />
-                </Layout>
-              )}
-            </MuiThemeProvider>
-          </RuiThemeProvider>
-        </MobxProvider>
-      </ComponentsProvider>
+      <RoutingProvider>
+        <UIProvider>
+          <AuthProvider>
+            <CartProvider>
+              <LocaleProvider
+                lang={lang}
+                translations={translations}
+                namespaces={namespaces}
+              >
+                <ShopProvider shop={shop}>
+                  <ComponentsProvider value={components}>
+                    <MobxProvider suppressChangedStoreWarning navItems={shop && shop.defaultNavigationTree} tags={tags}>
+                      <RuiThemeProvider theme={componentTheme}>
+                        <MuiThemeProvider theme={theme}>
+                          <CssBaseline />
+                            <Layout shop={shop}>
+                              <Component shop={shop} {...rest} {...pageProps} />
+                            </Layout>
+                        </MuiThemeProvider>
+                      </RuiThemeProvider>
+                    </MobxProvider>
+                  </ComponentsProvider>
+                </ShopProvider>
+              </LocaleProvider>
+            </CartProvider>
+          </AuthProvider>
+        </UIProvider>
+      </RoutingProvider>
     );
   }
 }
 
-// export default withMobX(withShop(withViewer(App)));
-export default withMobX(withShop(App));
+export default withMobX(App);
