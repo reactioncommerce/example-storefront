0a96fd44b1fa7cee809dc563165e4ccd7b9e8963
Janus Reith <janus.reith@googlemail.com>
2020-03-29 19:11:38 +0200

    use new graphql request wrapper function

--- staticUtils/shop/fetchPrimaryShop.js
+++ staticUtils/shop/fetchPrimaryShop.js
@@ -1,10 +1,8 @@
-import { request } from "graphql-request";
+import graphQLRequest from "staticUtils/graphQLRequest";
 import primaryShopQuery from "./primaryShop.js";
 
 export default async function fetchPrimaryShop(language) {
-  const endpoint = process.env.NODE_ENV === "production" ? process.env.EXTERNAL_GRAPHQL_URL : process.env.INTERNAL_GRAPHQL_URL;
-
-  const data = await request(endpoint, primaryShopQuery, { language });
+  const data = await graphQLRequest(primaryShopQuery, { language });
 
   return data && data.primaryShop && { shop: data.primaryShop };
 }
