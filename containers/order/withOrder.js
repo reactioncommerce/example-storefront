8c3caa890f276b7ee7b156847171ace2b6f0b0a1
Janus Reith <janus.reith@googlemail.com>
2020-03-30 01:46:57 +0200

    Implement order/orders as hook, use in withOrder HOC
    Signed-off-by: Janus Reith <mail@janusreith.de>

--- containers/order/withOrder.js
+++ containers/order/withOrder.js
@@ -1,10 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { Query } from "@apollo/react-components";
-import inject from "hocs/inject";
 import hoistNonReactStatic from "hoist-non-react-statics";
-import { orderByReferenceId } from "./queries.gql";
-import { useRouter } from "next/router";
+import useOrder from "../../hooks/orders/useOrder";
 
 /**
  * withOrder higher order query component for fetching an order
@@ -14,34 +10,18 @@ import { useRouter } from "next/router";
  */
 export default function withOrder(Component) {
   function WithOrder(props) {
-    const { primaryShopId, routingStore, uiStore } = props;
-    const { query } = useRouter();
-
-    const variables = {
-      id: query.orderId,
-      language: uiStore.language,
-      shopId: primaryShopId,
-      token: query.token || null
-    };
+    const [ order, isLoading ] = useOrder();
 
     return (
-      <Query errorPolicy="all" query={orderByReferenceId} variables={variables}>
-        {({ loading: isLoading, data: orderData }) => {
-          const { order } = orderData || {};
-
-          return (
-            <Component
-              {...props}
-              isLoadingOrder={isLoading}
-              order={order}
-            />
-          );
-        }}
-      </Query>
+      <Component
+        {...props}
+        isLoadingOrder={isLoading}
+        order={order}
+      />
     );
   }
 
   hoistNonReactStatic(WithOrder, Component);
 
-  return inject("cartStore", "primaryShopId", "routingStore", "uiStore")(WithOrder);
+  return WithOrder;
 }
