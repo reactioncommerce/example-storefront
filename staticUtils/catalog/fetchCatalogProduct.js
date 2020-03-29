87f094b9ee5f1c21a9695d79a02b3c13edf56d27
Janus Reith <janus.reith@googlemail.com>
2020-03-29 19:11:38 +0200

    use new graphql request wrapper function

--- staticUtils/catalog/fetchCatalogProduct.js
+++ staticUtils/catalog/fetchCatalogProduct.js
@@ -1,12 +1,10 @@
-import { request } from "graphql-request";
+import graphQLRequest from "staticUtils/graphQLRequest";
 import catalogItemProductQuery from "./catalogItemProduct.js";
 
 export default async function fetchCatalogProduct(slugOrId) {
   if (!slugOrId) return { product: {} };
 
-  const endpoint = process.env.NODE_ENV === "production" ? process.env.EXTERNAL_GRAPHQL_URL : process.env.INTERNAL_GRAPHQL_URL;
-
-  const data = await request(endpoint, catalogItemProductQuery, {
+  const data = await graphQLRequest(catalogItemProductQuery, {
     slugOrId
   });
 
