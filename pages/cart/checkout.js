214cdfe91e8ece5d6c3dbf6d0d6fa8cda87de47d
Janus Reith <janus.reith@googlemail.com>
2020-03-29 12:05:35 +0200

    Move to functional and seperate properly instead of rendering  by route

--- pages/cart/checkout.js
+++ pages/cart/checkout.js
@@ -1,33 +1,30 @@
-import React, { Component, Fragment } from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import Router from "next/router";
-import Helmet from "react-helmet";
-import { withStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
+import { useApolloClient } from "@apollo/client";
+import Head from "next/head";
+import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import CartEmptyMessage from "@reactioncommerce/components/CartEmptyMessage/v1";
 import CheckoutActions from "components/CheckoutActions";
 import Layout from "components/Layout";
-import CheckoutEmailAddress from "@reactioncommerce/components/CheckoutEmailAddress/v1";
-import CheckoutTopHat from "@reactioncommerce/components/CheckoutTopHat/v1";
-import Entry from "components/Entry";
-import ShopLogo from "@reactioncommerce/components/ShopLogo/v1";
-import CartIcon from "mdi-material-ui/Cart";
-import ChevronLeftIcon from "mdi-material-ui/ChevronLeft";
-import LockIcon from "mdi-material-ui/Lock";
-import Link from "components/Link";
-import CheckoutSummary from "components/CheckoutSummary";
 import PageLoading from "components/PageLoading";
-import withCart from "containers/cart/withCart";
-import withAvailablePaymentMethods from "containers/payment/withAvailablePaymentMethods";
 import { withApollo } from "lib/apollo/withApollo";
-import definedPaymentMethods from "../custom/paymentMethods";
+import useCart from "hooks/cart/useCart";
+import useStores from "hooks/useStores";
+import useShop from "hooks/shop/useShop";
+import useAvailablePaymentMethods from "hooks/availablePaymentMethods/useAvailablePaymentMethods";
+import useAddressValidation from "hooks/address/useAddressValidation";
+import useTranslation from "hooks/useTranslation";
+import definedPaymentMethods from "custom/paymentMethods";
 
-const styles = (theme) => ({
+import fetchPrimaryShop from "staticUtils/shop/fetchPrimaryShop";
+
+const useStyles = makeStyles((theme) => ({
   checkoutActions: {
     width: "100%",
-    maxWidth: "600px",
-    alignSelf: "flex-end",
+    maxWidth: "1440px",
+    alignSelf: "center",
     [theme.breakpoints.up("md")]: {
       paddingRight: "2rem"
     }
@@ -42,35 +39,19 @@ const styles = (theme) => ({
   checkoutContent: {
     flex: "1",
     maxWidth: theme.layout.mainContentMaxWidth,
-    padding: "1rem"
+    padding: "1rem",
+    [theme.breakpoints.down("md")]: {
+      maxWidth: "100%"
+    }
   },
   checkoutContentContainer: {
     display: "flex",
     justifyContent: "center"
   },
-  checkoutTitleContainer: {
-    alignSelf: "flex-end",
-    width: "8rem",
-    [theme.breakpoints.up("md")]: {
-      width: "10rem"
-    }
-  },
-  checkoutTitle: {
-    fontSize: "1.125rem",
-    color: theme.palette.reaction.black35,
-    display: "inline",
-    marginLeft: "0.3rem"
-  },
+
   flexContainer: {
     display: "flex",
     flexDirection: "column"
-  },
-  headerContainer: {
-    display: "flex",
-    justifyContent: "space-between",
-    margin: "0 auto",
-    maxWidth: theme.layout.mainContentMaxWidth,
-    padding: "1rem"
   },
   emptyCartContainer: {
     display: "flex",
@@ -86,302 +67,154 @@ const styles = (theme) => ({
   },
   logo: {
     color: theme.palette.reaction.reactionBlue,
-    marginRight: theme.spacing(),
+    marginRight: theme.spacing(1),
     borderBottom: `solid 5px ${theme.palette.reaction.reactionBlue200}`
-  },
-  // login view styles
-  backLink: {
-    "color": theme.palette.reaction.black80,
-    "cursor": "pointer",
-    "fontFamily": theme.typography.fontFamily,
-    "fontSize": 14,
-    "&:hover": {
-      color: theme.palette.reaction.reactionBlue400
-    }
-  },
-  backLinkText: {
-    letterSpacing: "0.3px",
-    lineHeight: 1.71,
-    marginLeft: theme.spacing(),
-    textDecoration: "underline"
-  },
-  headerFlex: {
-    alignSelf: "center",
-    flex: "1 1 1%"
-  },
-  header: {
-    alignContent: "center",
-    borderBottom: `solid 1px ${theme.palette.reaction.black10}`,
-    display: "flex",
-    justifyContent: "center",
-    marginBottom: theme.spacing(3),
-    padding: theme.spacing(3)
   },
   main: {
     flex: "1 1 auto",
     maxWidth: theme.layout.mainLoginMaxWidth,
     minHeight: "calc(100vh - 135px)",
     margin: "0 auto",
-    padding: theme.spacing(3, 3, 0),
+    padding: `${theme.spacing(3)}px ${theme.spacing(3)}px 0`,
     [theme.breakpoints.up("md")]: {
-      padding: theme.spacing(10, 3, 0)
+      padding: `${theme.spacing(10)}px ${theme.spacing(3)}px 0`
     }
   },
   root: {}
-});
+}));
 
-const hasIdentityCheck = (cart) => !!((cart && cart.account !== null) || (cart && cart.email));
+const Checkout = ({ router }) => {
+  const classes = useStyles();
+  const { cartStore } = useStores();
+  const shop = useShop();
+  const { locale, t } = useTranslation("common");
+  const apolloClient = useApolloClient();
+  const [addressValidation, addressValidationResults] = useAddressValidation();
 
-class Checkout extends Component {
-  static propTypes = {
-    availablePaymentMethods: PropTypes.arrayOf(PropTypes.shape({
-      name: PropTypes.string.isRequired
-    })),
-    cart: PropTypes.shape({
-      account: PropTypes.object,
-      checkout: PropTypes.object,
-      email: PropTypes.string,
-      items: PropTypes.array
-    }),
-    cartStore: PropTypes.object,
-    checkoutMutations: PropTypes.object,
-    classes: PropTypes.object,
-    clearAuthenticatedUsersCart: PropTypes.func,
-    hasMoreCartItems: PropTypes.bool,
-    isLoadingAvailablePaymentMethods: PropTypes.bool,
-    isLoadingCart: PropTypes.bool,
-    loadMoreCartItems: PropTypes.func,
-    onChangeCartItemsQuantity: PropTypes.func,
-    onRemoveCartItems: PropTypes.func,
-    router: PropTypes.object,
-    setEmailOnAnonymousCart: PropTypes.func,
-    shop: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      description: PropTypes.string
-    }),
-    theme: PropTypes.object.isRequired
-  };
+  const {
+    cart,
+    isLoadingCart,
+    setEmailOnAnonymousCart,
+    checkoutMutations,
+    clearAuthenticatedUsersCart,
+    hasMoreCartItems,
+    loadMoreCartItems,
+    onRemoveCartItems,
+    onChangeCartItemsQuantity
+  } = useCart();
 
-  state = {};
+  const [availablePaymentMethods = [], isLoadingAvailablePaymentMethods] = useAvailablePaymentMethods();
 
-  componentDidMount() {
-    this.handleRouteChange();
-  }
+  const { asPath } = router;
+  const hasIdentity = !!((cart && cart.account !== null) || (cart && cart.email));
+  const pageTitle = hasIdentity ? `Checkout | ${shop && shop.name}` : `Login | ${shop && shop.name}`;
 
-  componentDidUpdate() {
-    this.handleRouteChange();
-  }
-
-  /**
-   *
-   * @name handleRouteChange
-   * @summary Determines which /cart route (/cart/login || /cart/checkout) to display.
-   * @return {undefined}
-   */
-  handleRouteChange = () => {
-    const { cart } = this.props;
+  useEffect(() => {
     // Skipping if the `cart` is not available
     if (!cart) return;
-    if (hasIdentityCheck(cart) && this.pagePath === "/cart/login") {
-      Router.replaceRoute("/cart/checkout", {}, { shallow: true });
-    } else if (!hasIdentityCheck(cart) && this.asPath === "/cart/checkout") {
-      Router.replaceRoute("/cart/login", {}, { shallow: true });
+    if (!hasIdentity) {
+      // Router.push("/[lang]/cart/login", `/${locale}/cart/login`);
+      Router.push("/cart/login", `/cart/login`);
     }
-  };
+  }), [cart, hasIdentity, asPath, Router];
 
-  handleCartEmptyClick = () => Router.push("/");
-
-  get pagePath() {
-    return this.props.router.asPath;
-  }
-
-  /**
-   *
-   * @name hasIdentity
-   * @summary `true` if a customer is signed in or has set a "guest email" on their cart.
-   * @return {Boolean} - true if cart.account or cart.email are set.
-   */
-  get hasIdentity() {
-    const { cart } = this.props;
-    return hasIdentityCheck(cart);
-  }
-
-  // render page head
-  renderCheckoutHead() {
-    const { shop } = this.props;
-    const pageTitle = this.hasIdentity ? `Checkout | ${shop && shop.name}` : `Login | ${shop && shop.name}`;
-    return <Helmet title={pageTitle} meta={[{ name: "description", content: shop && shop.description }]} />;
-  }
-
-  // render page top bar
-  renderCheckoutTopHat() {
-    return <CheckoutTopHat checkoutMessage="Free Shipping + Free Returns" />;
-  }
-
-  // render page header
-  renderCheckoutHeader() {
-    return this.hasIdentity ? this.renderCheckoutActionsHeader() : this.renderLoginHeader();
-  }
-
-  renderLoginHeader() {
-    const { classes, shop } = this.props;
-
-    return (
-      <div className={classes.header}>
-        <div className={classes.headerFlex}>
-          <Link route="/" className={classes.backLink}>
-            <ChevronLeftIcon style={{ fontSize: 18, color: "inherit", verticalAlign: "sub", transition: "none" }} />
-            <span className={classes.backLinkText}>Back</span>
-          </Link>
-        </div>
-
-        <Link route="home">
-          <div className={classes.logo}>
-            {shop ? <ShopLogo shopName={shop.name} /> : "Example Storefront"}
-          </div>
-        </Link>
-
-        <div className={classes.headerFlex} />
-      </div>
-    );
-  }
-
-  renderCheckoutActionsHeader() {
-    const { classes, shop, theme } = this.props;
-    return (
-      <div className={classes.headerContainer}>
-        <Link route="home">
-          <div className={classes.logo}>
-            {shop ? <ShopLogo shopName={shop.name} /> : "Example Storefront"}
-          </div>
-        </Link>
-        <div className={classes.checkoutTitleContainer}>
-          <LockIcon
-            style={{
-              fontSize: 14,
-              color: theme.palette.reaction.black35
-            }}
-          />
-          <Typography className={classes.checkoutTitle}>Checkout</Typography>
-        </div>
-        <Link route="cart">
-          <CartIcon />
-        </Link>
-      </div>
-    );
-  }
-
-  renderCheckoutContent() {
-    const { cart, router: { asPath } } = this.props;
+  const renderCheckoutContent = () => {
     // sanity check that "tries" to render the correct /cart view if SSR doesn't provide the `cart`
-    if (!cart) return asPath === "/cart/checkout" ? this.renderCheckoutActions() : this.renderCheckoutLogin();
-    return this.hasIdentity ? this.renderCheckoutActions() : this.renderCheckoutLogin();
-  }
 
-  // render page content: login || checkout
-  renderCheckoutLogin() {
-    const { classes, setEmailOnAnonymousCart } = this.props;
-
-    return (
-      <main className={classes.main}>
-        <Entry setEmailOnAnonymousCart={setEmailOnAnonymousCart} />
-      </main>
-    );
-  }
-
-  renderCheckoutActions() {
-    const {
-      availablePaymentMethods,
-      classes,
-      cart,
-      cartStore,
-      checkoutMutations,
-      clearAuthenticatedUsersCart,
-      hasMoreCartItems,
-      loadMoreCartItems,
-      onRemoveCartItems,
-      onChangeCartItemsQuantity
-    } = this.props;
-
-    if (!cart || (cart && Array.isArray(cart.items) && cart.items.length === 0)) {
+    if (!cart) {
       return (
         <div className={classes.emptyCartContainer}>
           <div className={classes.emptyCart}>
             <div>
-              <CartEmptyMessage onClick={this.handleCartEmptyClick} />
+              <CartEmptyMessage onClick={() => Router.push("/")} messageText="Ihr Warenkorb ist leer." buttonText="Weiter einkaufen" />
             </div>
           </div>
         </div>
       );
     }
 
-    const hasAccount = !!cart.account;
-    const orderEmailAddress = (hasAccount && Array.isArray(cart.account.emailRecords) && cart.account.emailRecords[0].address) || cart.email;
-
-    // Filter the hard-coded definedPaymentMethods list from the client to remove any
-    // payment methods that were not returned from the API as currently available.
-    const paymentMethods = definedPaymentMethods.filter((method) =>
-      !!availablePaymentMethods.find((availableMethod) => availableMethod.name === method.name));
-
-    return (
-      <div className={classes.checkoutContentContainer}>
-        <div className={classes.checkoutContent}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={7}>
-              <div className={classes.flexContainer}>
-                <div className={classes.checkoutActions}>
-                  {orderEmailAddress ? (
-                    <CheckoutEmailAddress emailAddress={orderEmailAddress} isAccountEmail={hasAccount} />
-                  ) : null}
-                  <CheckoutActions
-                    cart={cart}
-                    cartStore={cartStore}
-                    checkoutMutations={checkoutMutations}
-                    clearAuthenticatedUsersCart={clearAuthenticatedUsersCart}
-                    orderEmailAddress={orderEmailAddress}
-                    paymentMethods={paymentMethods}
-                  />
-                </div>
+    if (hasIdentity && cart) {
+      if (cart && Array.isArray(cart.items) && cart.items.length === 0) {
+        return (
+          <div className={classes.emptyCartContainer}>
+            <div className={classes.emptyCart}>
+              <div>
+                <CartEmptyMessage onClick={() => Router.push("/")} messageText="Ihr Warenkorb ist leer." buttonText="Weiter einkaufen" />
               </div>
-            </Grid>
-            <Grid item xs={12} md={5}>
-              <div className={classes.flexContainer}>
-                <div className={classes.cartSummary}>
-                  <CheckoutSummary
-                    cart={cart}
-                    hasMoreCartItems={hasMoreCartItems}
-                    onRemoveCartItems={onRemoveCartItems}
-                    onChangeCartItemsQuantity={onChangeCartItemsQuantity}
-                    onLoadMoreCartItems={loadMoreCartItems}
-                  />
+            </div>
+          </div>
+        );
+      }
+
+      const orderEmailAddress = (cart && cart.account && Array.isArray(cart.account.emailRecords) && cart.account.emailRecords[0].address) || (cart ? cart.email : null);
+
+      // Filter the hard-coded definedPaymentMethods list from the client to remove any
+      // payment methods that were not returned from the API as currently available.
+      const paymentMethods = definedPaymentMethods.filter((method) =>
+        !!availablePaymentMethods.find((availableMethod) => availableMethod.name === method.name));
+
+      return (
+        <div className={classes.checkoutContentContainer}>
+          <div className={classes.checkoutContent}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={12}>
+                <div className={classes.flexContainer}>
+                  <div className={classes.checkoutActions}>
+                    <CheckoutActions
+                      locale={locale}
+                      apolloClient={apolloClient}
+                      addressValidation={addressValidation}
+                      addressValidationResults={addressValidationResults}
+                      cart={cart}
+                      cartStore={cartStore}
+                      checkoutMutations={checkoutMutations}
+                      clearAuthenticatedUsersCart={clearAuthenticatedUsersCart}
+                      orderEmailAddress={orderEmailAddress}
+                      paymentMethods={paymentMethods}
+                      hasMoreCartItems={hasMoreCartItems}
+                      onRemoveCartItems={onRemoveCartItems}
+                      onChangeCartItemsQuantity={onChangeCartItemsQuantity}
+                      onLoadMoreCartItems={loadMoreCartItems}
+                    />
+                  </div>
                 </div>
-              </div>
+              </Grid>
             </Grid>
-          </Grid>
+          </div>
         </div>
-      </div>
-    );
-  }
-
-  render() {
-    const {
-      isLoadingCart,
-      isLoadingAvailablePaymentMethods
-    } = this.props;
-
-    if (isLoadingCart || isLoadingAvailablePaymentMethods) {
-      return <PageLoading delay={0} />;
+      );
     }
+  };
 
+  if (isLoadingCart || isLoadingAvailablePaymentMethods) {
     return (
       <Layout shop={shop}>
-        {this.renderCheckoutHead()}
-        {this.renderCheckoutTopHat()}
-        {this.renderCheckoutHeader()}
-        {this.renderCheckoutContent()}
+        <PageLoading delay={0} />
       </Layout>
     );
   }
+
+  return (
+    <Layout shop={shop}>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="description" content={shop && shop.description} />
+      </Head>
+      {renderCheckoutContent()}
+    </Layout>
+  );
+};
+
+Checkout.propTypes = {
+  router: PropTypes.object
+};
+
+export async function getStaticProps() {
+  return {
+    props: {
+      ...await fetchPrimaryShop()
+    }
+  };
 }
 
-export default withApollo()(withCart(withAvailablePaymentMethods(withStyles(styles, { withTheme: true })(Checkout))));
+export default withApollo()(Checkout);
