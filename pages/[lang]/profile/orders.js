3193347d9004205cc53241d96ca3f24c802894f2
Janus Reith <janus.reith@googlemail.com>
2020-03-30 14:23:13 +0200

    only fetch all tags on pages using breadcrumbs - Todo: Get rif of fetchAllTags altogether

--- pages/[lang]/profile/orders.js
+++ pages/[lang]/profile/orders.js
@@ -12,7 +12,6 @@ import { withApollo } from "lib/apollo/withApollo";
 
 import { locales } from "translations/config";
 import fetchPrimaryShop from "staticUtils/shop/fetchPrimaryShop";
-import fetchAllTags from "staticUtils/tags/fetchAllTags";
 import fetchTranslations from "staticUtils/translations/fetchTranslations";
 
 import ErrorPage from "pages/_error";
@@ -82,8 +81,7 @@ export async function getStaticProps({ params: { lang } }) {
   return {
     props: {
       ...await fetchPrimaryShop(lang),
-      ...await fetchTranslations(lang, ["common"]),
-      ...await fetchAllTags(lang)
+      ...await fetchTranslations(lang, ["common"])
     }
   };
 }
