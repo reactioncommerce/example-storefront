fdbff4339ea28d9eaaab03d40a1e7b93daa8068c
Janus Reith <janus.reith@googlemail.com>
2020-03-30 02:22:10 +0200

    add wrapper for useTranslation
    Signed-off-by: Janus Reith <mail@janusreith.de>

---
+++ hocs/withTranslation.js
@@ -0,0 +1,21 @@
+import useTranslation from "hooks/useTranslation";
+
+const withTranslation = (namespace) => (PageComponent) => {
+  const WithInjectedStores = (props) => {
+    const { locale, t } = useTranslation(namespace);
+
+    return (
+      <PageComponent {...props} locale={locale} t={t} />
+    );
+  };
+
+    // Set the correct displayName in development
+  if (process.env.NODE_ENV !== "production") {
+    const displayName = PageComponent.displayName || PageComponent.name || "Component";
+    WithInjectedStores.displayName = `withInjectedStores(${displayName})`;
+  }
+
+  return WithInjectedStores;
+};
+
+export default withTranslation;
