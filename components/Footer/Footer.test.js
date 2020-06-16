686192d66e60cc7d27154edbd832c134344a279b
Will Lopez <will.lopez@mailchimp.com>
2020-06-16 15:43:46 -0700

    refactor: use screen in test
    Signed-off-by: Will Lopez <will.lopez@mailchimp.com>

--- components/Footer/Footer.test.js
+++ components/Footer/Footer.test.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Footer from "./Footer";
 
 test("Renders the footer", () => {
-  const { getByText } = render(<Footer />);
-  const footerText = getByText(/Reaction Commerce/);
+  render(<Footer />);
+
+  const footerText = screen.getByText(/Reaction Commerce/);
   expect(footerText).toBeInTheDocument();
 });
