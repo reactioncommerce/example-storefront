d0b235b3041c814b23655d38ca1cbf3490012c8f
Will Lopez <will.lopez@mailchimp.com>
2020-06-16 13:28:49 -0700

    chore: configure react-testing-library and add a sample test Signed-off-by: Will Lopez <will.lopez@mailchimp.com>

---
+++ components/Footer/Footer.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Footer from "./Footer";
+
+test("Renders the footer", () => {
+  const { getByText } = render(<Footer />)
+  const footerText = getByText(
+    /Reaction Commerce/
+  )
+  expect(footerText).toBeInTheDocument()
+})
