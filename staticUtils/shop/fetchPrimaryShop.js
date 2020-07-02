b62cc32564fe94046bb0afa9d9d4304046dab49e
Mike Murray <hellomikemurray@gmail.com>
2020-07-02 10:27:11 -0700

    fix: return null shop to fix destructuring issues
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- staticUtils/shop/fetchPrimaryShop.js
+++ staticUtils/shop/fetchPrimaryShop.js
@@ -10,5 +10,5 @@ import primaryShopQuery from "./primaryShop.js";
 export default async function fetchPrimaryShop(language) {
   const data = await graphQLRequest(primaryShopQuery, { language });
 
-  return data && data.primaryShop && { shop: data.primaryShop };
+  return (data && data.primaryShop && { shop: data.primaryShop }) || { shop: null };
 }
