1aa98dc5882bf57cb2867df23bf478372986ca18
Alvaro Bueno <alvaro.bueno@mailchimp.com>
2021-08-18 16:46:52 -0400

    fix: keep stripe method
    Signed-off-by: Alvaro Bueno <alvaro.bueno@mailchimp.com>

--- custom/paymentMethods.js
+++ custom/paymentMethods.js
@@ -1,19 +1,21 @@
-// import ExampleIOUPaymentForm from "@reactioncommerce/components/ExampleIOUPaymentForm/v1";
-// import StripePaymentInput from "@reactioncommerce/components/StripePaymentInput/v1";
-
+import ExampleIOUPaymentForm from "@reactioncommerce/components/ExampleIOUPaymentForm/v1";
+import StripePaymentInput from "@reactioncommerce/components/StripePaymentInput/v1";
 import StripeCard from "components/StripeCard";
-
 const paymentMethods = [
-  /*
   {
     displayName: "IOU",
     InputComponent: ExampleIOUPaymentForm,
     name: "iou_example",
     shouldCollectBillingAddress: true
-  }
-  */
+  },
   {
     displayName: "Credit Card",
+    InputComponent: StripePaymentInput,
+    name: "stripe_card",
+    shouldCollectBillingAddress: true
+  },
+  {
+    displayName: "Credit Card (SCA)",
     InputComponent: StripeCard,
     name: "stripe_payment_intent",
     shouldCollectBillingAddress: true
