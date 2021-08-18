1669dc83ff5c22c13f4aa4e0124eb5f3a004781e
Alvaro Bueno <alvaro.bueno@mailchimp.com>
2021-08-18 18:14:13 -0400

    fix: lint
    Signed-off-by: Alvaro Bueno <alvaro.bueno@mailchimp.com>

--- custom/paymentMethods.js
+++ custom/paymentMethods.js
@@ -1,6 +1,7 @@
 import ExampleIOUPaymentForm from "@reactioncommerce/components/ExampleIOUPaymentForm/v1";
 import StripePaymentInput from "@reactioncommerce/components/StripePaymentInput/v1";
 import StripeCard from "components/StripeCard";
+
 const paymentMethods = [
   {
     displayName: "IOU",
