2488d0790f078ca7cc9653d3b4ae45dc504c8aec
Chloe <pinkcloudvnn@gmail.com>
2022-05-26 15:53:09 +0700

    fix: primary shop query
    Signed-off-by: Chloe <pinkcloudvnn@gmail.com>

--- staticUtils/shop/fetchPrimaryShop.js
+++ staticUtils/shop/fetchPrimaryShop.js
@@ -1,5 +1,5 @@
-import graphQLRequest from "staticUtils/graphQLRequest";
-import primaryShopQuery from "./primaryShop.js";
+import client from "../../lib/apollo/apolloClient";
+import primaryShopQuery from "./primaryShop.gql";
 
 /**
  * Fetch the primary shop's information
@@ -8,7 +8,12 @@ import primaryShopQuery from "./primaryShop.js";
  * @returns {Object} The primary shop
  */
 export default async function fetchPrimaryShop(language) {
-  const data = await graphQLRequest(primaryShopQuery, { language });
+  const { data } = await client.query({
+    query: primaryShopQuery,
+    variables: {
+      language,
+    },
+  });
 
-  return (data && data.primaryShop && { shop: data.primaryShop }) || { shop: null };
+  return data?.primaryShop ? { shop: data.primaryShop } : { shop: null };
 }
